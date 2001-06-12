9f155b3a7d886cd8d94a9bd0b045ac4fc8d019f0
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-06-12 22:10:43 +0000

    Added build.sh for building simkit on Unix and Unix-like systems

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 if not exist simsystem mkdir simsystem
-javac -verbose -d simsystem graph\*.java simkit\*.java simkit\data\*.java simkit\random\*.java simkit\smd\*.java simkit\util\*.java
+javac -O -verbose -d simsystem graph\*.java simkit\*.java simkit\data\*.java simkit\random\*.java simkit\smd\*.java simkit\util\*.java
 copy simkit\*.txt simsystem\simkit
 cd simsystem
 jar cvfM simsystem.zip graph simkit
 cd ..
+java -cp simsystem\simsystem.zip simkit.Version
 pause
---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ -d simsystem ]; then
+	echo "directory simsystem exists"
+else
+	if [ -e simsystem]; then 
+		rm simsystem
+	fi
+	echo "directory simsystem doesn't exist"
+	mkdir simsystem
+fi
+javac -O -classpath . -verbose -d simsystem graph/*.java simkit/*.java simkit/data/*.java simkit/random/*.java simkit/smd/*.java simkit/util/*.java
+cp simkit/*.txt simsystem/simkit
+cd simsystem
+jar cvfM simsystem.zip graph simkit
+cd ..
+java -cp simsystem/simsystem.zip simkit.Version
+
+
