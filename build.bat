86943db76ce80b732bf36cdc375c27492fda9928
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-06-25 19:35:45 +0000

    Ficed build.bat

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@ javac -O -verbose -d simsystem graph\*.java simkit\*.java simkit\data\*.java sim
 copy simkit\*.txt simsystem\simkit
 cd simsystem
 jar cvfM simsystem.zip graph simkit
+if not exist doc mkdir doc
 cd ..
+javadoc -author -private -d simsystem\doc simkit simkit.data simkit.smd simkit.stat simkit.random simkit.util
 java -cp simsystem\simsystem.zip simkit.Version
 pause
