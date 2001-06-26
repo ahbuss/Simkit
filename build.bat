dd7a7447035844e6e8c413177407ae82c3c789e9
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-06-26 16:44:36 +0000

    build.bat should now work :-)

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@echo off
 if not exist simsystem mkdir simsystem
 javac -O -verbose -d simsystem graph\*.java simkit\*.java simkit\data\*.java simkit\random\*.java simkit\smd\*.java simkit\util\*.java
 copy simkit\*.txt simsystem\simkit
