aad7aa6a4513bc50fd205ad21bf76469b3fa00dd
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-06-26 16:48:05 +0000

    Simple histogram

---
+++ src/simkit/stat/Histogram.java
@@ -0,0 +1,54 @@
+package simkit.stat;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
+public class Histogram implements PropertyChangeListener {
+
+    private String name;
+    private int[] frequency;
+    private int total;
+
+    public Histogram(String name, int number) {
+        this.name = name;
+        frequency = new int[number + 1];
+    }
+
+    public void propertyChange(PropertyChangeEvent e) {
+        if (e.getPropertyName().equals(name) ){
+            frequency[ (Math.min(((Number) e.getNewValue()).intValue(), frequency.length - 1))]++;
+            total++;
+        }
+    }
+
+    public void reset() {
+        frequency = new int[frequency.length];
+        total = 0;
+    }
+
+    public int[] getFrequencies() { return (int[]) frequency.clone(); }
+
+    public double[] getProportions() {
+        double[] temp = new double[frequency.length];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = frequency[i] / (double) total;
+        }
+        return temp;
+    }
+
+    public String toString() {
+        StringBuffer buf = new StringBuffer(name + "\nindex\tFrequencies\tProportions\n");
+        double[] prop = getProportions();
+        for (int i = 0; i < frequency.length; i++ ) {
+            buf.append(i);
+            buf.append('\t');
+            buf.append(frequency[i]);
+            buf.append('\t');
+            buf.append(prop[i]);
+            buf.append('\n');
+        }
+        return buf.toString();
+
+    }
+
+}
