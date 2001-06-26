55aa4a4e957b58ab915af5f1e444a7a842e665a8
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-06-26 16:51:25 +0000

    Exponential transform enhances any RandomVariate LKSeeds are from Law and Kelton generator 100,000 numbers apart

---
+++ src/simkit/random/ExponentialTransform.java
@@ -0,0 +1,26 @@
+package simkit.random;
+
+public class ExponentialTransform implements RandomVariate {
+
+    private RandomVariate rv;
+
+    public ExponentialTransform(RandomVariate rvariate) {
+        rv = RandomVariateFactory.getInstance(rvariate);
+    }
+
+    public double generate() {
+        return Math.exp(rv.generate());
+    }
+
+    public void setSeed(long seed) {rv.getRandomNumber().setSeed(seed);}
+    public void resetSeed() {rv.getRandomNumber().resetSeed();}
+
+    public long getSeed() { return rv.getRandomNumber().getSeed(); }
+
+     public Object[] getParameters() {return rv.getParameters();}
+
+    public void setParameters(Object[] params) {rv.setParameters(params);}
+
+    public RandomNumber getRandomNumber() { return rv.getRandomNumber(); }
+
+    public void setRandomNumber(RandomNumber rng) { rv.setRandomNumber(rng); }}
