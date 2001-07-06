27360394aca679e21f7f6a02d3c95ab1bffc8f19
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-07-06 07:37:15 +0000

    Updated.

--- src/simkit/test/TestSimpleStatsTally.java
+++ src/simkit/test/TestSimpleStatsTally.java
@@ -1,17 +1,45 @@
 package simkit.test;
 
-import simkit.stat.*;
+import simkit.Schedule;
+import simkit.data.SimpleStats;
+import simkit.examples.ArrivalProcess;
+import simkit.stat.SimpleStatsTally;
+import simkit.random.RandomVariateFactory;
 
 public class TestSimpleStatsTally {
 
     public static void main(String[] args) {
         SimpleStatsTally sst = new SimpleStatsTally();
         SimpleStats ss = new SimpleStats();
-        for (int i = 1; i < 1000001; i++) {
+        for (int i = 1; i < 100001; i++) {
             sst.newObservation(i);
             ss.newObservation(i);
         }
         System.out.println(sst);
         System.out.println(ss);
+
+        ss.setName("numberArrivals");
+        sst.setName("numberArrivals");
+        sst.reset();
+        ss.reset();
+
+        ArrivalProcess ap =
+            new ArrivalProcess(
+                RandomVariateFactory.getInstance(
+                    "simkit.random.ExponentialVariate",
+                    new Object[] { new Double(1.7) },
+                    12345L
+                )
+            );
+        ap.addPropertyChangeListener(ss);
+        ap.addPropertyChangeListener(sst);
+
+        Schedule.stopAtTime(20.0);
+        Schedule.reset();
+        Schedule.startSimulation();
+
+        System.out.println(sst);
+        System.out.println(ss);
+        System.out.println(sst.clone());
     }
 } 
