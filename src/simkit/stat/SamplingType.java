a4d5a0c461f3342bd7eb879f48fdd4878bfcfed6
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-07-06 06:57:41 +0000

    Add comments?

--- src/simkit/stat/SamplingType.java
+++ src/simkit/stat/SamplingType.java
@@ -1,10 +1,16 @@
 package simkit.stat;
-
+/**
+ *  A Java-style "enumeration" class to represent the type of sampling being
+ *  done by a given class.  Current options are TALLY and TIME_VARYING.  LINEAR
+ *  is not yet implemented.
+ *  @author Arnold Buss
+ *  @version $Date$ 
+**/
 public class SamplingType {
    protected SamplingType(String s){ name = s;}
    public static SamplingType TALLY = new SamplingType("TALLY");
    public static SamplingType TIME_VARYING = new SamplingType("TIME_VARYING");
-//   public static SamplingType LINEAR = new SamplingType("LINEAR");
+   public static SamplingType LINEAR = new SamplingType("LINEAR");
    private String name;
    public String toString(){return name;}
 }
