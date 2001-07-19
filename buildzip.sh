83eccd5ae133887449b18107f56043455674f062
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-07-19 18:29:27 +0000

    Added simkit/stat to build scripts

--- buildzip.sh
+++ buildzip.sh
@@ -9,7 +9,7 @@ else
 	echo "directory simsystem doesn't exist"
 	mkdir simsystem
 fi
-javac -O -classpath . -verbose -d simsystem graph/*.java simkit/*.java simkit/data/*.java simkit/random/*.java simkit/smd/*.java simkit/util/*.java simkit/examples/*.java
+javac -O -classpath . -verbose -d simsystem graph/*.java simkit/*.java simkit/data/*.java simkit/random/*.java simkit/stat/*.java simkit/smd/*.java simkit/util/*.java simkit/examples/*.java
 cp simkit/*.txt simsystem/simkit
 cd simsystem
 jar cvfM simsystem.zip graph simkit
