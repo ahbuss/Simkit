3e8e3cb6a67b2d6096c989680f79fb8d3d05e1f0
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-08-27 16:14:37 +0000

    Test for SimpleStatsTimeVarying reset() method at simTime > 0.0

---
+++ src/simkit/test/Resetter.java
@@ -0,0 +1,69 @@
+/*
+ * Resetter.java
+ *
+ * Created on August 25, 2001, 12:57 PM
+ */
+
+package simkit.test;
+
+import simkit.SimEntityBase;
+import simkit.Schedule;
+import simkit.examples.ArrivalProcess;
+import simkit.stat.SimpleStatsTimeVarying;
+import simkit.random.RandomVariate;
+import simkit.random.RandomVariateFactory;
+
+/**
+ *
+ * @author  ahbuss
+ * @version
+ */
+public class Resetter extends SimEntityBase {
+    
+    private int state;
+    private double resetTime;
+    
+    /** Creates new Resetter */
+    public Resetter(double resetTime) {
+        this.resetTime = resetTime;
+    }
+    
+    public void reset() {
+        super.reset();
+        state = 0;
+    }
+    
+    public void doRun() {
+        firePropertyChange("state", state);
+        waitDelay("Reset", resetTime);
+    }
+    
+    public void doArrival() {
+        firePropertyChange("state", state, ++state);
+    }
+    
+    public void doReset() {
+        firePropertyChange("reset", "state");
+    }
+    
+    public static void main(String[] args) {
+        ArrivalProcess arrival = new ArrivalProcess("simkit.random.ConstantVariate",
+        new Object[] { new Double(1.0) }, 1L);
+        Resetter r = new Resetter(5.0);
+        SimpleStatsTimeVarying sstv = new SimpleStatsTimeVarying("state");
+        SimpleStatsTimeVarying sstv2 = new SimpleStatsTimeVarying("numberArrivals");
+        
+        arrival.addSimEventListener(r);
+        r.addPropertyChangeListener(sstv);
+        arrival.addPropertyChangeListener(sstv2);
+        
+        Schedule.setSingleStep(true);
+        Schedule.stopAtTime(10.0);
+        Schedule.reset();
+        Schedule.startSimulation();
+        
+        System.out.println(sstv);
+        System.out.println(sstv2);
+    }
+    
+}
