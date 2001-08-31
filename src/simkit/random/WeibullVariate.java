140f76f22b026cbfba4e2d8a1bb719fd71717c76
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-08-31 04:10:16 +0000

    Cleaned up error checking of parameters

--- src/simkit/random/WeibullVariate.java
+++ src/simkit/random/WeibullVariate.java
@@ -1,79 +1,33 @@
 package simkit.random;
 
-
-
 public class WeibullVariate extends RandomVariateBase{
-
-
-
     private double alpha;
-
     private double alphaInverse;
-
     private double beta;
-
-
-
+    
     public WeibullVariate() {
-
     }
-
-
-
     public double generate() {
-
         return beta * Math.pow(- Math.log(rng.draw()), alphaInverse);
-
     }
-
-
-
     public void setParameters(Object[] params) {
-
         if (params.length != 2) {
-
-            throw new IllegalArgumentException("Must bave two parameters for Weibull");
-
+            throw new IllegalArgumentException("Must bave two parameters for Weibull, "
+            + params.length + " given");
         }
-
-        String err = "";
-
-        if (!(params[0] instanceof Number)) {
-
-            err += "First parameter is " + params[0].getClass().getName();
-
+        if (params[0] instanceof Number && params[1] instanceof Number) {
+            alpha = ((Number) params[0]).doubleValue();
+            alphaInverse = 1.0 / alpha;
+            beta = ((Number) params[1]).doubleValue();
         }
-
-        if (!(params[1] instanceof Number)) {
-
-            err = "Second parameter is " + params[1].getClass().getName();
-
+        else {            
+            throw new IllegalArgumentException("For Weibull both parameters must "+
+            " be numbers: (" + params[0].getClass().getName() + ", " +
+            params[1].getClass().getName() + ") given" );
         }
-
-        if (!err.equals("")) {
-
-            throw new IllegalArgumentException(err);
-
-        }
-
-        alpha = ((Number) params[0]).doubleValue();
-
-        alphaInverse = 1.0 / alpha;
-
-        beta = ((Number) params[1]).doubleValue();
-
     }
-
-
-
     public Object[] getParameters() {
-
-        return new Object[] {new Double(alpha),  new Double(beta)};      
-
+        return new Object[] {new Double(alpha),  new Double(beta)};
     }
-
-
-
-    public String toString() { return "Weibull (" + beta + ", " + alpha + ")"; } 
-
-} 
+    public String toString() { return "Weibull (" + beta + ", " + alpha + ")"; }
+}
