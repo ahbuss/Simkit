f1c0d4e71537f937d30f8e2ec07c2e639c438f3f
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-08-31 03:39:19 +0000

    Uniform sample - useful for bootstrapping?

---
+++ src/simkit/random/UniformSampleVariate.java
@@ -0,0 +1,54 @@
+/*
+ * UniformSampleVariate.java
+ *
+ * Created on August 30, 2001, 8:32 PM
+ */
+
+package simkit.random;
+
+/**
+ *
+ * @author  ahbuss
+ * @version 
+ */
+public class UniformSampleVariate extends simkit.random.RandomVariateBase {
+
+    private double[] sample;
+    
+    /** Creates new UniformSampleVariate */
+    public UniformSampleVariate() {
+    }
+
+    /**
+     * Generate a random variate having this class's distribution.
+ */
+    public double generate() {
+        return 0.0;
+    }
+    
+    /**
+     * Returns the array of parameters as an Object[].
+ */
+    public Object[] getParameters() {
+        return new Object[] { sample.clone() };
+    }
+    
+    /**
+     * Sets the random variate's parameters.
+     * Alternatively, the parameters could be set in the constructor or
+     * in additional methods provided by the programmer.
+     * @param params the array of parameters, wrapped in objects.
+ */
+    public void setParameters(Object[] params) {
+        if (params.length != 1) {
+            throw new IllegalArgumentException();
+        }
+        if (params[0] instanceof double[]) {
+            sample = (double[]) ((double[]) params[0]).clone();
+        }
+        else if (params[0] instanceof int[]) {
+            sample = (double[]) ((int[])params[0]).clone();
+        }
+    }
+    
+}
