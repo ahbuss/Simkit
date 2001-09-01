b366857e5751f12d4a98bd79401f5fffe830ec0e
arnie <arnie@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-09-01 02:37:35 +0000

    Deleted set/get for parameters - these must be implemented in the subclass

--- src/simkit/random/RandomVariateBase.java
+++ src/simkit/random/RandomVariateBase.java
@@ -23,25 +23,6 @@ public abstract class RandomVariateBase implements RandomVariate {
         rng = RandomNumberFactory.getInstance();
     }
 
-/*
- *  Sets the random variate's parameters.
- *  Alternatively, the parameters could be set in the constructor or
- *  in additional methods provided by the programmer.
- *  @param params the array of parameters, wrapped in objects.
-
-    public void setParameters(Object[] params) {
-        if (params != null) {
-            parameters = (Object[]) params.clone();
-        }
-    }
-*/
-/*
- *  @return the array of parameters as an Object[].
-
-    public Object[] getParameters() {
-        return (parameters == null) ? new Object[]{} : (Object[]) parameters.clone();
-    }
-*/
 /**
  *  Use this if another random number generator besides the default is desired
  *  @param gen The <CODE>RandomNumber</CODE> instance to use for Un(0, 1) random numbers.
