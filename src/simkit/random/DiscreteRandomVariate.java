4e603c88cce5dfaf35731cbef78a92197a69d51f
arnie <arnie@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-09-01 02:31:49 +0000

    Added DiscreteRandomVariate interface

---
+++ src/simkit/random/DiscreteRandomVariate.java
@@ -0,0 +1,19 @@
+/*
+ * DiscreteRandomVariate.java
+ *
+ * Created on August 31, 2001, 7:14 PM
+ */
+
+package simkit.random;
+
+/**
+ *
+ * @author  Arnold Buss
+ * @version 
+ */
+public interface DiscreteRandomVariate extends RandomVariate {
+    
+    public int generateInt();
+
+}
+
