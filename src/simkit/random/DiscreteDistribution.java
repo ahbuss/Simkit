eb0c1f979ffefc9196321f1a38e7d9611447289f
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-09-19 23:53:12 +0000

    Interface for discrete probability distribution

---
+++ src/simkit/random/DiscreteDistribution.java
@@ -0,0 +1,23 @@
+/*
+ * DiscreteDistribution.java
+ *
+ * Created on September 19, 2001, 4:42 PM
+ */
+
+package simkit.random;
+
+/**
+ *
+ * @author  Arnold Buss
+ * @version 
+ */
+public interface DiscreteDistribution extends Distribution {
+    
+    public double pmf(double x);
+    
+    public int[] getValues();
+    
+    public double[] getRealValues();
+    
+}
+
