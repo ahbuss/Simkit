3008c1e2179a404602831b0938107cc4335380fa
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-09-20 16:41:06 +0000

    Since RandomVariate now implements Cloneable, boilerplate public clone() method implemented in base class.

--- src/simkit/random/RandomVariateBase.java
+++ src/simkit/random/RandomVariateBase.java
@@ -5,33 +5,45 @@ package simkit.random;
  *  abstract because the <CODE>generate()</CODE> method of <CODE>RandomVariate</CODE>
  *  is not implemented.  The easiest way to write a <CODE>RandomVariate</CODE> is to
  *  subclass and implement <CODE>generate()</CODE>.  It is also advised that subclasses
- *  add accessor methods for their parameters using the set/get template. 
- *  
- *  <p>The set/get parameters really should be implemented in the subclass, so these have been 
+ *  add accessor methods for their parameters using the set/get template.
+ *
+ *  <p>The set/get parameters really should be implemented in the subclass, so these have been
  *  removed. 31 Aug 2001.
  *
  *  @author Arnold Buss
- *  @version 1.2.1 
-**/
+ *  @version 1.2.1
+ **/
 
 public abstract class RandomVariateBase implements RandomVariate {
-
+    
     protected RandomNumber rng;
-//    private Object[] parameters ;
-
+    //    private Object[] parameters ;
+    
     public RandomVariateBase() {
         rng = RandomNumberFactory.getInstance();
     }
-
-/**
- *  Use this if another random number generator besides the default is desired
- *  @param gen The <CODE>RandomNumber</CODE> instance to use for Un(0, 1) random numbers.
-**/
+    
+    /**
+     *  Use this if another random number generator besides the default is desired
+     *  @param gen The <CODE>RandomNumber</CODE> instance to use for Un(0, 1) random numbers.
+     **/
     public void setRandomNumber(RandomNumber rng) { this.rng = rng; }
-
-/**
- *  @return The <CODE>RandomNumber</CODE> instance currently being used.
-**/
+    
+    /**
+     *  @return The <CODE>RandomNumber</CODE> instance currently being used.
+     **/
     public RandomNumber getRandomNumber() { return rng; }
-
+    
+    /**
+     *  @return a copy of the RandomVariate instance;
+     **/
+    
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {}
+        finally {}
+        return null;        
+    }
+    
 }
