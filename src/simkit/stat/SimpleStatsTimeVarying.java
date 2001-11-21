2152cc22988eb0fce66bcebe4a34d08715ad1ac1
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-11-21 23:54:30 +0000

    Now extends AbstractSimpleStats

--- src/simkit/stat/SimpleStatsTimeVarying.java
+++ src/simkit/stat/SimpleStatsTimeVarying.java
@@ -5,37 +5,24 @@ import simkit.Schedule;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
 
-public class SimpleStatsTimeVarying implements SampleStatistics, PropertyChangeListener {
+public class SimpleStatsTimeVarying extends AbstractSimpleStats {
     
-    private static String DEFAULT_NAME = "%unnamed%";
-    private static String DEFAULT_FORMAT = " 0.0000;-0.0000";
-    
-    private int count;
     private double mean;
     private double variance;
-    private double minObs;
-    private double maxObs;
     private double diff;
     
     private double lastTime;
     private double startTime;
     
-    private String name;
-    private DecimalFormat df;
-    
     public SimpleStatsTimeVarying(String name) {
-        this.setFormat(DEFAULT_FORMAT);
-        this.setName(name);
-        this.reset();
+        super(name);
     }
     
     public SimpleStatsTimeVarying() {
         this(DEFAULT_NAME);
     }
     public void newObservation(double x) {
-        minObs = (x < minObs) ? x : minObs;
-        maxObs = (x > maxObs) ? x : maxObs;
-        count++;
+        super.newObservation(x);
         if (count == 1 ) {
             mean = diff;
             variance = 0.0;
@@ -48,14 +35,6 @@ public class SimpleStatsTimeVarying implements SampleStatistics, PropertyChangeL
         this.lastTime = Schedule.getSimTime();
     }
     
-    public void newObservation(Number x) { this.newObservation( x.doubleValue() ); }
-    
-    public void newObservation(boolean b) { this.newObservation(b ? 1.0 : 0.0); }
-    
-    public void newObservation(Boolean b) { this.newObservation(b.booleanValue() ? 1.0 : 0.0); }
-    
-    public double getMinObs() { return minObs; }
-    public double getMaxObs() { return maxObs; }
     public double getMean() {
         if (Schedule.getSimTime() > this.lastTime) {
             newObservation(diff + mean);
@@ -69,81 +48,17 @@ public class SimpleStatsTimeVarying implements SampleStatistics, PropertyChangeL
         return variance;
     }
     public double getStandardDeviation() { return Math.sqrt(getVariance()); }
-    public int getCount() { return count; }
-    
-    public void setFormat(String formatString) {
-        df = new DecimalFormat(formatString);
-    }
-    
-    // Implements Named interface
-    
-    public void setName(String newName) { this.name = newName; }
-    public String getName() { return name; }
-    
+   
     public SamplingType getSamplingType() { return SamplingType.TIME_VARYING; }
     
     public void setSamplingType(SamplingType type) {  }
     
-    public void propertyChange(PropertyChangeEvent event) {
-        if (this.getName().equals(event.getPropertyName()) ){
-            Object value = event.getNewValue();
-            if (value instanceof Number) {
-                this.newObservation((Number)event.getNewValue());
-            }
-            else if (value instanceof Boolean) {
-                this.newObservation((Boolean) value);
-            }
-        }
-        else if (event.getPropertyName().equals("reset") &&
-                this.getName().equals(event.getNewValue())) {
-            this.reset();
-        }
-    }
-    
     public void reset() {
-        count = 0;
+        super.reset();
         diff = 0.0;
         mean = Double.NaN;
         variance = Double.NaN;
-        minObs = Double.POSITIVE_INFINITY;
-        maxObs = Double.NEGATIVE_INFINITY;
         lastTime = Schedule.getSimTime();
         startTime = Schedule.getSimTime();        
-    }
-    
-    public synchronized Object clone() {
-        try {
-            return super.clone();
-        } catch (CloneNotSupportedException e) {}
-        return null;
-    }
-    
-    public String getDataLine() {
-        StringBuffer buf = new StringBuffer();
-        buf.append(getCount());
-        buf.append(' ');
-        buf.append(df.format(getMinObs()));
-        buf.append(' ');
-        buf.append(df.format(getMaxObs()));
-        buf.append(' ');
-        buf.append(df.format(getMean()));
-        buf.append(' ');
-        buf.append(df.format(getVariance()));
-        buf.append(' ');
-        buf.append(df.format(getStandardDeviation()));
-        
-        return buf.toString();
-    }
-    
-    public String toString() {
-        StringBuffer buf = new StringBuffer();
-        buf.append(getName());
-        buf.append(' ');
-        buf.append('(');
-        buf.append(this.getSamplingType());
-        buf.append(')');
-        buf.append(EOL);
-        
-        return buf.toString() + getDataLine();
     }
 }
