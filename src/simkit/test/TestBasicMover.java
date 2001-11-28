f0568f062e439d5ba639c630b387b6ca71c87cc5
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-11-28 01:36:54 +0000

    no message

--- src/simkit/test/TestBasicMover.java
+++ src/simkit/test/TestBasicMover.java
@@ -12,28 +12,63 @@ import simkit.smd.Coordinate;
  *  works properly.
  */
 public class TestBasicMover {
-
+    
     /**
     * @param args the command line arguments
     */
     public static void main (String args[]) {
-        BasicMover bm = new BasicMover("Test (10, 20) 50");
+        VariableSpeedMover bm = new VariableSpeedMover("Test (10, 20) 50");
         Coordinate destination = new Coordinate("40 60");
         
         Schedule.setVerbose(true);
+        
+//  Move to destination at maximum speed (should take 1.0)
         Schedule.reset();
         bm.moveTo(destination);
         Schedule.startSimulation();
         
+//  Move to destination at speed less than maximum speed (should take 2.5)
         Schedule.reset();
         bm.moveTo(destination, 20);
         Schedule.startSimulation();
         
+//  Attempt to move to destination at speed greater than max speed (should take 1.0)
         Schedule.reset();
         bm.moveTo(destination, 100);
         Schedule.startSimulation();
         
+//  Attempt to move to destination at speed less than max speed 
+//  by first setting the speed then using as a second argument to
+//  moveTo(Coordinate, double) (should take 2.0)
+        Schedule.reset();
+        bm.setSpeed(25);
+        bm.moveTo(destination, bm.getSpeed());
+        Schedule.startSimulation();
     }
+}
+
+/**
+ *  Class to illustrate subclassing BasicMover so setSpeed() can be made public.
+ *  Note that you cannot simply setSpeed() then moveTo(), but must use
+ *  moveTo(Coordinate, VariableSpeedMover.getSpeed());
+ */
+    class VariableSpeedMover extends BasicMover {
+        
+        public VariableSpeedMover(String name, Coordinate location, double maxSpeed) {
+            super(name, location, maxSpeed);
+        }
+        
+        public VariableSpeedMover(Coordinate location, double speed) {
+            super(location, speed);
+        }
+        
+        public VariableSpeedMover(String params) {
+            super(params);
+        }
+        
+        public void setSpeed(double speed) { super.setSpeed(speed); }
+    }
+    
 /*
  ** Event List -- Starting Simulation **
 0.000        StartMove         {Test (10.00, 20.00) 50.0}        
@@ -77,6 +112,18 @@ Time: 1.000         Current Event: EndMove         {Test (40.00, 60.00) 50.0}   
                << empty >>
  ** End  of Event List --  **
  
+ ** Event List -- Starting Simulation **
+0.000        StartMove         {Test (10.00, 20.00) 25.0}        
+ ** End  of Event List -- Starting Simulation **
+ 
+Time: 0.000         Current Event: StartMove         {Test (10.00, 20.00) 25.0}        [1]
+ ** Event List --  **
+2.000        EndMove         {Test (10.00, 20.00) 25.0}        
+ ** End  of Event List --  **
+ 
+Time: 2.000         Current Event: EndMove         {Test (40.00, 60.00) 25.0}        [1]
+ ** Event List --  **
+               << empty >>
+ ** End  of Event List --  **
+ 
  */    
-
-}
