96a1e41ee36110ff0031e6d6d358aa44b1a2eb05
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2001-11-28 01:40:47 +0000

    no message

--- src/simkit/test/TestBasicMover.java
+++ src/simkit/test/TestBasicMover.java
@@ -7,39 +7,39 @@ import simkit.smd.Coordinate;
 /**
  *
  * @author  Arnold Buss
- * 
+ *
  *  This test verifies that the BasicMover,moveTo(Coordinate, double) method
  *  works properly.
  */
 public class TestBasicMover {
     
     /**
-    * @param args the command line arguments
-    */
-    public static void main (String args[]) {
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
         VariableSpeedMover bm = new VariableSpeedMover("Test (10, 20) 50");
         Coordinate destination = new Coordinate("40 60");
         
         Schedule.setVerbose(true);
         
-//  Move to destination at maximum speed (should take 1.0)
+        //  Move to destination at maximum speed (should take 1.0)
         Schedule.reset();
         bm.moveTo(destination);
         Schedule.startSimulation();
         
-//  Move to destination at speed less than maximum speed (should take 2.5)
+        //  Move to destination at speed less than maximum speed (should take 2.5)
         Schedule.reset();
         bm.moveTo(destination, 20);
         Schedule.startSimulation();
         
-//  Attempt to move to destination at speed greater than max speed (should take 1.0)
+        //  Attempt to move to destination at speed greater than max speed (should take 1.0)
         Schedule.reset();
         bm.moveTo(destination, 100);
         Schedule.startSimulation();
         
-//  Attempt to move to destination at speed less than max speed 
-//  by first setting the speed then using as a second argument to
-//  moveTo(Coordinate, double) (should take 2.0)
+        //  Attempt to move to destination at speed less than max speed
+        //  by first setting the speed then using as a second argument to
+        //  moveTo(Coordinate, double) (should take 2.0)
         Schedule.reset();
         bm.setSpeed(25);
         bm.moveTo(destination, bm.getSpeed());
@@ -52,31 +52,42 @@ public class TestBasicMover {
  *  Note that you cannot simply setSpeed() then moveTo(), but must use
  *  moveTo(Coordinate, VariableSpeedMover.getSpeed());
  */
-    class VariableSpeedMover extends BasicMover {
-        
-        public VariableSpeedMover(String name, Coordinate location, double maxSpeed) {
-            super(name, location, maxSpeed);
-        }
-        
-        public VariableSpeedMover(Coordinate location, double speed) {
-            super(location, speed);
-        }
-        
-        public VariableSpeedMover(String params) {
-            super(params);
-        }
-        
-        public void setSpeed(double speed) { super.setSpeed(speed); }
+class VariableSpeedMover extends BasicMover {
+    
+    /**
+     * @param name
+     * @param location
+     * @param maxSpeed  */
+    public VariableSpeedMover(String name, Coordinate location, double maxSpeed) {
+        super(name, location, maxSpeed);
     }
     
+    /**
+     * @param location
+     * @param speed  */
+    public VariableSpeedMover(Coordinate location, double speed) {
+        super(location, speed);
+    }
+    
+    /**
+     * @param params  */
+    public VariableSpeedMover(String params) {
+        super(params);
+    }
+    
+    /**
+     * @param speed  */
+    public void setSpeed(double speed) { super.setSpeed(speed); }
+}
+
 /*
  ** Event List -- Starting Simulation **
-0.000        StartMove         {Test (10.00, 20.00) 50.0}        
+0.000        StartMove         {Test (10.00, 20.00) 50.0}
  ** End  of Event List -- Starting Simulation **
  
 Time: 0.000         Current Event: StartMove         {Test (10.00, 20.00) 50.0}        [1]
  ** Event List --  **
-1.000        EndMove         {Test (10.00, 20.00) 50.0}        
+1.000        EndMove         {Test (10.00, 20.00) 50.0}
  ** End  of Event List --  **
  
 Time: 1.000         Current Event: EndMove         {Test (40.00, 60.00) 50.0}        [1]
@@ -85,12 +96,12 @@ Time: 1.000         Current Event: EndMove         {Test (40.00, 60.00) 50.0}   
  ** End  of Event List --  **
  
  ** Event List -- Starting Simulation **
-0.000        StartMove         {Test (10.00, 20.00) 20.0}        
+0.000        StartMove         {Test (10.00, 20.00) 20.0}
  ** End  of Event List -- Starting Simulation **
  
 Time: 0.000         Current Event: StartMove         {Test (10.00, 20.00) 20.0}        [1]
  ** Event List --  **
-2.500        EndMove         {Test (10.00, 20.00) 20.0}        
+2.500        EndMove         {Test (10.00, 20.00) 20.0}
  ** End  of Event List --  **
  
 Time: 2.500         Current Event: EndMove         {Test (40.00, 60.00) 20.0}        [1]
@@ -99,12 +110,12 @@ Time: 2.500         Current Event: EndMove         {Test (40.00, 60.00) 20.0}   
  ** End  of Event List --  **
  
  ** Event List -- Starting Simulation **
-0.000        StartMove         {Test (10.00, 20.00) 50.0}        
+0.000        StartMove         {Test (10.00, 20.00) 50.0}
  ** End  of Event List -- Starting Simulation **
  
 Time: 0.000         Current Event: StartMove         {Test (10.00, 20.00) 50.0}        [1]
  ** Event List --  **
-1.000        EndMove         {Test (10.00, 20.00) 50.0}        
+1.000        EndMove         {Test (10.00, 20.00) 50.0}
  ** End  of Event List --  **
  
 Time: 1.000         Current Event: EndMove         {Test (40.00, 60.00) 50.0}        [1]
@@ -113,12 +124,12 @@ Time: 1.000         Current Event: EndMove         {Test (40.00, 60.00) 50.0}   
  ** End  of Event List --  **
  
  ** Event List -- Starting Simulation **
-0.000        StartMove         {Test (10.00, 20.00) 25.0}        
+0.000        StartMove         {Test (10.00, 20.00) 25.0}
  ** End  of Event List -- Starting Simulation **
  
 Time: 0.000         Current Event: StartMove         {Test (10.00, 20.00) 25.0}        [1]
  ** Event List --  **
-2.000        EndMove         {Test (10.00, 20.00) 25.0}        
+2.000        EndMove         {Test (10.00, 20.00) 25.0}
  ** End  of Event List --  **
  
 Time: 2.000         Current Event: EndMove         {Test (40.00, 60.00) 25.0}        [1]
@@ -126,4 +137,4 @@ Time: 2.000         Current Event: EndMove         {Test (40.00, 60.00) 25.0}   
                << empty >>
  ** End  of Event List --  **
  
- */    
+ */
