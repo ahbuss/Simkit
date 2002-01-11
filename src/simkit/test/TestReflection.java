6ead4eba295f525ce5be9b732b59bc2d977fef17
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-01-11 21:37:36 +0000

    no message

---
+++ src/simkit/test/TestReflection.java
@@ -0,0 +1,34 @@
+/*
+ * TestReflection.java
+ *
+ * Created on December 7, 2001, 3:16 PM
+ */
+
+package simkit.test;
+
+import java.lang.reflect.*;
+import simkit.util.*;
+/**
+ *
+ * @author  ahbuss
+ * @version 
+ */
+public class TestReflection {
+
+    /** Creates new TestReflection */
+    public TestReflection() {
+    }
+    
+    public void foo(Double x, Integer y) {}
+
+    /**
+    * @param args the command line arguments
+    */
+    public static void main (String args[]) throws Throwable {
+        Object[] arg = new Object[] { new Double(1.0), new Integer(3) };
+        String name = Misc.getFullMethodName(TestReflection.class, "foo", arg);
+        System.out.println(name);
+        
+    }
+
+}
