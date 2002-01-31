945559b2164a2e5f3f8b10c609a5470061452efc
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-01-31 02:25:12 +0000

    no message

---
+++ src/simkit/test/TestDiscreteVariate.java
@@ -0,0 +1,52 @@
+/*
+ * Main.java
+ *
+ * Created on January 30, 2002, 6:03 PM
+ */
+
+package simkit.test;
+
+import java.beans.PropertyChangeSupport;
+
+import simkit.random.RandomVariate;
+import simkit.random.RandomVariateFactory;
+import simkit.stat.Histogram;
+
+/**
+ *
+ * @author  Arnold Buss
+ * @version
+ */
+public class TestDiscreteVariate {
+    
+    /** Creates new Main */
+    public TestDiscreteVariate() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        RandomVariate rv =
+        RandomVariateFactory.getInstance(
+        "simkit.random.DiscreteVariate",
+        new Object[] { new double[] {0, 1}, new double[] { 0.5, 0.5}},
+        12345L
+        );
+        
+        System.out.println(rv);
+        
+        Histogram h = new Histogram("discrete", 1);
+        PropertyChangeSupport pcs = new PropertyChangeSupport(rv);
+        pcs.addPropertyChangeListener("discrete", h);
+        
+        
+        for (int i = 0; i < 10000; i++) {
+            pcs.firePropertyChange("discrete", null, new Double(rv.generate()));
+        }
+        
+        System.out.println(h);
+        
+    }
+    
+}
