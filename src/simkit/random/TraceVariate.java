5e2367e877c93b22069820ccaf30d1a0c155ee73
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-02-23 03:14:29 +0000

    Added possible second argument giving the "default" value - i.e. the value returned when the array runs out of values

--- src/simkit/random/TraceVariate.java
+++ src/simkit/random/TraceVariate.java
@@ -1,21 +1,26 @@
 package simkit.random;
 
 public class TraceVariate extends RandomVariateBase {
-
+    
     private double[] traceValues;
-
+    private double defaultValue;
+    
     public TraceVariate() {
         this.setRandomNumber(new Sequential());
     }
-
+    
     public void setTraceValues(double[] values) {
-        traceValues = (double[]) values.clone() ; 
+        traceValues = (double[]) values.clone() ;
     }
     
     public double[] getTraceValues() { return (double[]) traceValues.clone(); }
-
+    
+    public void setDefaultValue(double value) { defaultValue = value; }
+    
+    public double getDefaultValue() { return defaultValue; }
+    
     public void setParameters(Object[] params) {
-        if (params.length != 1) {
+        if (params.length != 1 || params.length != 2) {
             throw new IllegalArgumentException("Need parameters length 1: " + params.length);
         }
         if (params[0] instanceof double[]) {
@@ -24,13 +29,25 @@ public class TraceVariate extends RandomVariateBase {
         else {
             throw new IllegalArgumentException("Need type double[]: " + params[0].getClass().getName());
         }
+        if (params.length == 2) {
+            if (params[1] instanceof Number) {
+                setDefaultValue(((Number) params[1]).doubleValue());
+            }
+        }
     }
-
-    public Object[] getParameters() { return new Object[] { getTraceValues() }; }
+    
+    public Object[] getParameters() {
+        if (Double.isNaN(defaultValue)) {
+            return new Object[] { getTraceValues() }; 
+        }
+        else {
+            return new Object[] { getTraceValues(), new Double(getDefaultValue()) };
+        }
+    }
     
     public double generate() {
-        double value = (rng.getSeed() < traceValues.length) ? traceValues[(int) rng.getSeed() ] : Double.NaN;
+        double value = (rng.getSeed() < traceValues.length) ? traceValues[(int) rng.getSeed() ] : getDefaultValue();
         rng.draw();
         return value;
     }
-} 
+}
