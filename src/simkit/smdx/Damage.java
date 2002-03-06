b3a941594797e4f70ab76f62a2b57e1d91cba72a
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-03-06 22:27:06 +0000

    no message

---
+++ src/simkit/smdx/Damage.java
@@ -0,0 +1,19 @@
+/*
+ * Damage.java
+ *
+ * Created on February 20, 2002, 5:25 PM
+ */
+
+package simkit.smdx;
+
+/**
+ *
+ * @author  Arnold Buss
+ * @version 
+ */
+public interface Damage {
+    
+    public Object getDamage();
+
+}
+
---
+++ src/simkit/smdx/Moveable.java
@@ -0,0 +1,23 @@
+/*
+ * Moveable.java
+ *
+ * Created on February 20, 2002, 12:57 AM
+ */
+
+package simkit.smdx;
+import java.awt.geom.*;
+/**
+ *
+ * @author  Arnold Buss
+ * @version 
+ */
+public interface Moveable {
+    
+    public Point2D getCurrentLocation();
+    
+    public Point2D getVelocity();
+    
+    public Point2D getAcceleration();
+
+}
+
