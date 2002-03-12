35f807023c24baf152755a6eb06294e8ac45b65c
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-03-12 06:04:35 +0000

    no message

--- src/simkit/SimEntityComparator.java
+++ src/simkit/SimEntityComparator.java
@@ -24,8 +24,8 @@ public class SimEntityComparator implements Comparator {
         
         if (firstSimEntity.getPriority() < secondSimEntity.getPriority()) { return 1; }
         if (firstSimEntity.getPriority() > secondSimEntity.getPriority()) { return -1; }
-        if (firstSimEntity.getSerial() < secondSimEntity.getSerial()) { return 1; }
-        if (firstSimEntity.getSerial() > secondSimEntity.getSerial()) { return -1;} 
+        if (firstSimEntity.getSerial() < secondSimEntity.getSerial()) { return -1; }
+        if (firstSimEntity.getSerial() > secondSimEntity.getSerial()) { return 1;} 
                
         return 0;
     }    
