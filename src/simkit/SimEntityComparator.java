68b49b800e3509e586eb88e2247edb0db0ac6399
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-03-12 05:41:07 +0000

    Comparator to sort SimEntities, so Schedule.reset() occurs in priority order (ties broken by serial).

---
+++ src/simkit/SimEntityComparator.java
@@ -0,0 +1,33 @@
+/*
+ * SimEntityComparator.java
+ *
+ * Created on March 11, 2002, 9:02 PM
+ */
+
+package simkit;
+import java.util.Comparator;
+/**
+ *
+ * @author  ahbuss
+ * @version 
+ */
+public class SimEntityComparator implements Comparator {
+
+    /** Creates new SimEntityComparator */
+    public SimEntityComparator() {
+    }
+
+    public int compare(java.lang.Object first, java.lang.Object second) {
+        if (first.equals(second) || second.equals(first)) { return 0; }
+        SimEntity firstSimEntity = (SimEntity) first;
+        SimEntity secondSimEntity = (SimEntity) second;
+        
+        if (firstSimEntity.getPriority() < secondSimEntity.getPriority()) { return 1; }
+        if (firstSimEntity.getPriority() > secondSimEntity.getPriority()) { return -1; }
+        if (firstSimEntity.getSerial() < secondSimEntity.getSerial()) { return 1; }
+        if (firstSimEntity.getSerial() > secondSimEntity.getSerial()) { return -1;} 
+               
+        return 0;
+    }    
+    
+}
