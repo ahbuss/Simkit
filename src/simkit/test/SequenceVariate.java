c51fba84426d481a4effb3153c3a0f7fcdf24e1b
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-03-15 07:39:29 +0000

    Sample to test RandomvariateFactory search paths

---
+++ src/simkit/test/SequenceVariate.java
@@ -0,0 +1,66 @@
+/*
+ * SequentialVariate.java
+ *
+ * Created on March 14, 2002, 11:17 PM
+ */
+
+package simkit.test;
+import simkit.random.*;
+/**
+ *
+ * @author  Arnold Buss
+ * @version 
+ */
+public class SequenceVariate implements RandomVariate {
+
+    private RandomNumber rng;
+    
+    /** Creates new SequentialVariate */
+    public SequenceVariate() {
+        rng = new Sequential();
+    }
+
+    /**
+     * @return The underlying RandomNumber instance (should be a copy)
+     */
+    public RandomNumber getRandomNumber() { return rng; }
+    
+    /** Sets the supporting RandomNumber object
+     * @param rng The RandomNumber instance supporting the generating algorithm
+     */
+    public void setRandomNumber(RandomNumber rng) { this.rng = rng; }
+    
+    /**
+     * Returns the array of parameters as an Object[].
+     * @return the array of parameters as an Object[].
+     */
+    public Object[] getParameters() {
+        return null;
+    }
+    
+    /**
+     * Sets the random variate's parameters.
+     * Alternatively, the parameters could be set in the constructor or
+     * in additional methods provided by the programmer.
+     * @param params the array of parameters, wrapped in objects.
+     */
+    public void setParameters(Object[] params) {
+    }
+    
+    /**
+     * Generate a random variate having this class's distribution.
+     * @return The generated random variate
+     */
+    public double generate() {
+        rng.draw();
+        return (double) rng.getSeed();
+    }
+    
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {}
+        return null;
+    }
+    
+}
