5c1e28ffdf27a13e9b61a29901e47133acf89b50
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-03-16 01:31:36 +0000

    no message

---
+++ src/simkit/test/TestAntithetic.java
@@ -0,0 +1,39 @@
+/*
+ * TestAntithetic.java
+ *
+ * Created on March 15, 2002, 4:41 PM
+ */
+
+package simkit.test;
+import simkit.random.*;
+
+/**
+ *
+ * @author  Arnold Buss
+ * @version 
+ */
+public class TestAntithetic {
+
+    /** Creates new TestAntithetic */
+    public TestAntithetic() {
+    }
+
+    /**
+    * @param args the command line arguments
+    */
+    public static void main (String args[]) {
+        RandomVariate rv = RandomVariateFactory.getInstance("Uniform",
+            new Object[] { new Double(0.0), new Double(1.0) });
+        RandomVariate copy = RandomVariateFactory.getInstance(rv);
+        RandomNumber anti = RandomNumberFactory.getAntithetic(copy.getRandomNumber());
+        copy.setRandomNumber(anti);
+        
+        for (int i = 0; i < 10; i++) {
+            double x = copy.generate();
+            System.out.println(rv.generate() + "\t" + x + "\t" + (1.0 - x));
+        }
+        
+        
+    }
+
+}
