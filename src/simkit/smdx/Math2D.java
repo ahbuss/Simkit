253ad854f1d25228e050569e864ee1b4a5c6fe8a
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-03-20 07:23:09 +0000

    Added line intersections

--- src/simkit/smdx/Math2D.java
+++ src/simkit/smdx/Math2D.java
@@ -15,22 +15,39 @@ import java.util.*;
  */
 public class Math2D {
 
-    public Point2D add(Point2D first, Point2D second) {
+    public static Point2D add(Point2D first, Point2D second) {
         return new Point2D.Double(
             first.getX() + second.getX(), first.getY() + second.getY()  );
     }
     
-    public Point2D subtract(Point2D first, Point2D second) {
+    public static Point2D subtract(Point2D first, Point2D second) {
         return new Point2D.Double(
             first.getX() - second.getX(), first.getY() - second.getY()  );
     }
     
-    public Point2D scalarMultiply(double value, Point2D point) {
+    public static Point2D scalarMultiply(double value, Point2D point) {
         return new Point2D.Double( value * point.getX(), value * point.getY());
     }
     
-    public double innerProduct(Point2D first, Point2D second) {
+    public static double innerProduct(Point2D first, Point2D second) {
         return first.getX() * second.getX() + first.getY() * second.getY();
+    }
+    
+    public static Point2D findIntersection(Point2D start, Point2D velocity, Line2D line) {
+        AffineTransform trans = getTransform(velocity, subtract(line.getP1(), line.getP2()));
+        if (trans.getDeterminant() != 0.0) {
+            try {
+                Point2D result = trans.inverseTransform(subtract(line.getP1(), start), null);
+                if (result.getX() >= 0.0 && result.getY() >= 0.0 && result.getY() <= 1.0) {
+                    return add( start, scalarMultiply(result.getX(), velocity));
+                }
+            } catch (NoninvertibleTransformException e) {}
+        }
+        return null;
+    }
+    
+    public static AffineTransform getTransform(Point2D p1, Point2D p2) {
+        return new AffineTransform(new double[] { p1.getX(), p1.getY(), p2.getX(), p2.getY()});
     }
     
 }
