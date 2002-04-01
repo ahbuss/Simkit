f4a330cd80d7e15fff50171b279876776bb28fb9
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-04-01 05:45:57 +0000

    no message

---
+++ src/simkit/test/TestNormal02Variate.java
@@ -0,0 +1,53 @@
+/*
+ * TestBinomialVariate.java
+ *
+ * Created on March 29, 2002, 4:22 PM
+ */
+
+package simkit.test;
+import simkit.random.*;
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author  Arnold Buss
+ * @version 
+ */
+public class TestNormal02Variate {
+
+    /** Creates new TestBinomialVariate */
+    public TestNormal02Variate() {
+    }
+
+    /**
+    * @param args the command line arguments
+    */
+    public static void main (String args[]) throws Throwable {
+        String dist = "Normal03Variate";
+        Object params[] = new Object[2];
+        params[0] = new Integer(0);
+        params[1] = new Integer(1);
+        RandomVariate rv = 
+            (RandomVariate) RandomVariateFactory.getInstance(dist, params);
+        
+        int number = args.length > 0 ? Integer.parseInt(args[0]) : 100000;
+        
+        URL dirURL = Thread.currentThread().getContextClassLoader().getSystemResource("simkit");
+        File dir = new File(dirURL.getFile()).getParentFile();
+        BufferedWriter out = new BufferedWriter(new FileWriter(new File(dir, "norm03.txt")));
+        
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < number; i++) {
+            buf.append(rv.generate());
+            buf.append(' ');
+            if ( (i + 1) % 100 == 0 ) {
+                out.write(buf.toString());
+                out.newLine(); 
+                buf = new StringBuffer();
+            }
+        }
+        out.close();        
+        System.out.println(rv);
+    }
+
+}
