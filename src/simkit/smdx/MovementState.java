934488dc597810e2862fee3a39aab56e09eb94bd
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-05-28 18:44:45 +0000

    Types of Movement state a Mover can be in

---
+++ src/simkit/smdx/MovementState.java
@@ -0,0 +1,36 @@
+/*
+ * MovementState.java
+ *
+ * Created on May 28, 2002, 12:32 PM
+ */
+
+package simkit.smdx;
+import java.util.*;
+
+/**
+ *
+ * @author  Arnold Buss
+ */
+public class MovementState {
+    
+    public static final MovementState PAUSED = new MovementState("Paused");
+    public static final MovementState STOPPED = new MovementState("Stopped");
+    public static final MovementState ACCELERATING = new MovementState("Accelerating");
+    public static final MovementState CRUISING = new MovementState("Cruising");
+    
+    private static HashMap states;
+    public static MovementState getState(String name) { return (MovementState) states.get(name); }
+        
+    private String name;
+    
+    protected MovementState(String name) {
+        this.name = name;
+        if (states == null) {
+            states = new HashMap(4);
+        }
+        states.put(name, this);
+    }
+    
+    public String toString() { return name; }
+    
+}
