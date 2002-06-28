50d32c3257e4bd3c3cbcccd21419d4afd3e1f7a9
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-06-28 23:24:04 +0000

    *** empty log message ***

---
+++ src/simkit/smdx/Platform.java
@@ -0,0 +1,20 @@
+/*
+ * Platform.java
+ *
+ * Created on June 28, 2002, 4:15 PM
+ */
+
+package simkit.smdx;
+
+/**
+ *
+ * @author  ahbuss
+ */
+public interface Platform extends Moveable {
+    
+    public boolean isAlive();
+    
+    public void kill();
+    
+    
+}
