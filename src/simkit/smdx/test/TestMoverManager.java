9568e73d2992427d4e6012035602b4cd49498f05
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-07-29 19:28:38 +0000

    Updated to test MoverManagerFactory

--- src/simkit/smdx/test/TestMoverManager.java
+++ src/simkit/smdx/test/TestMoverManager.java
@@ -23,14 +23,22 @@ public class TestMoverManager {
         RandomVariate[] location = new RandomVariate[2];
         location[0] = RandomVariateFactory.getInstance("Uniform",
         new Object[] { new Double(0.0), new Double(100.0) }, rng);
-        location[1] = RandomVariateFactory.getInstance("Uniform",   
-            new Object[] { new Double(0.0), new Double(100.0) }, rng);
+        location[1] = RandomVariateFactory.getInstance("Uniform",
+        new Object[] { new Double(0.0), new Double(100.0) }, rng);
         
-        Point2D start = new Point2D.Double(0.0, 0.0);            
-        Mover mover = new UniformLinearMover("Fred", start, 20.0);
-            
-        MoverManager manager = new RandomLocationMoverManager(mover, location);
-        manager.setStartOnReset(true);
+        Point2D start = new Point2D.Double(0.0, 0.0);
+        Mover[] mover = new Mover[2];
+        mover[0] = new UniformLinearMover("Fred", start, 20.0);
+        mover[1] = new UniformLinearMover("Barney", start, 30.0);
+        
+        MoverManager[] manager = new MoverManager[2];
+        manager[0] = new RandomLocationMoverManager(mover[0], location);
+        manager[1] = MoverManagerFactory.getInstance("simkit.smdx.RandomLocationMoverManager",
+            new Object[] { mover[1], location });
+        for (int i = 0; i < manager.length; i++) {
+            manager[i].setStartOnReset(true);
+        }
+        
         Schedule.setSingleStep(true);
         Schedule.reset();
         Schedule.startSimulation();
