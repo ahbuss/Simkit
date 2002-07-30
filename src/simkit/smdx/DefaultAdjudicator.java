e54af6992cc0392ff39927550ae9867bdbf1b1b4
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-07-30 19:07:14 +0000

    Do-nothing Adjudicator

---
+++ src/simkit/smdx/DefaultAdjudicator.java
@@ -0,0 +1,22 @@
+/*
+ * DefaultAdjudicator.java
+ *
+ * Created on July 29, 2002, 3:37 PM
+ */
+
+package simkit.smdx;
+
+/**
+ *
+ * @author  Arnold Buss
+ */
+public class DefaultAdjudicator implements Adjudicator {
+    
+    /** Does nothing.  Use as the default in many cases
+     * @param munition The Munition instance for the impact
+     * @param target The affected target
+     */    
+    public void adjudicate(Munition munition, Target target) {
+    }
+    
+}
