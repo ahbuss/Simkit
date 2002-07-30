147cac7a64139d069b7c06e0a13820621fbbf717
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-07-30 01:12:58 +0000

    Optionally dumps source

--- src/simkit/util/SimplePropertyDumper.java
+++ src/simkit/util/SimplePropertyDumper.java
@@ -8,14 +8,30 @@ package simkit.util;
 import java.beans.*;
 
 public class SimplePropertyDumper implements PropertyChangeListener {
+    
+    private boolean dumpSource;
 
+    public SimplePropertyDumper() {
+        this(false);
+    }
+    
+    public SimplePropertyDumper(boolean dumpSource) {
+        setDumpSource(dumpSource);
+    }
+    
     public void propertyChange(PropertyChangeEvent e) {
         String index = "";
         if (e instanceof IndexedPropertyChangeEvent) {
             index = "[" + ((IndexedPropertyChangeEvent) e).getIndex() + "]";
         }
-
-        System.out.println(e.getPropertyName() + index + ": " + e.getOldValue() + " => " +
+        if (isDumpSource()) {
+            System.out.print("{" + e.getSource() + "} " );
+        }
+        System.out.println( e.getPropertyName() + index + ": " + e.getOldValue() + " => " +
             e.getNewValue());
     }
+    
+    public void setDumpSource(boolean ds) { dumpSource = ds; }
+    
+    public boolean isDumpSource() { return dumpSource; }
 }
