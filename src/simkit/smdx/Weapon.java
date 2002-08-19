6854354da7ef0fe25ed97c88aae9e34d65b063c5
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-08-19 23:19:17 +0000

    *** empty log message ***

--- src/simkit/smdx/Weapon.java
+++ src/simkit/smdx/Weapon.java
@@ -1,20 +1,16 @@
 package simkit.smdx;
-import simkit.smd.Mover;
-import simkit.smd.Coordinate;
+
+import java.awt.geom.Point2D;
 import simkit.SimEntity;
 
 public interface Weapon extends Moveable, SimEntity {
-
-    public void fireAt(Target target);
 
     public void reload(int numberRounds);
 
     public int getRemainingRounds();
 
-    public void doShoot(Weapon weapon, Coordinate aimPoint);
+    public void doShoot(Munition munition, Point2D aimPoint);
     
-    public void doShoot(Weapon weapon, Target target);
-
     public void setMover(Moveable mover);
     
     public Moveable getMover();
