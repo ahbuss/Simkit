cc2695b30e543c4b90ba6848f787986dcf5d3973
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-08-22 01:16:15 +0000

    Added Comments

--- src/simkit/smdx/CookieCutterMediator.java
+++ src/simkit/smdx/CookieCutterMediator.java
@@ -1,19 +1,11 @@
-/*
- * CookieCutterMediator.java
- *
- * Created on March 6, 2002, 8:51 PM
- */
-
 package simkit.smdx;
 
 import java.beans.*;
 import simkit.*;
 import java.util.*;
 
-/**
- *
- * @author  Arnold Buss
- * @version
+/** Mediator for CookieCutter detection.
+ * @author Arnold Buss
  */
 public class CookieCutterMediator extends SimEntityBase implements SensorTargetMediator {
     
@@ -24,6 +16,16 @@ public class CookieCutterMediator extends SimEntityBase implements SensorTargetM
         contacts = new WeakHashMap();
     }
     
+    /** When this event is heard, schedule the detection for the sensor
+     * based on the detection algorithm - in this case, immediately.
+     * The sensor is passed an instance of a Contact, which is a 
+     * doppleganger for the actual target.  Note that the waitDelay()
+     * is invoked directly on the sensor.  This makes the sensor the
+     * "source" of the SimEvent, and allows listeners to the sensor to be
+     * able to hear it.
+     * @param sensor The sensor whose range was entered
+     * @param target The target that entered the sensor's range
+     */    
     public void doEnterRange(Sensor sensor, Mover target) {
         if (this == SensorTargetMediatorFactory.getInstance().getMediatorFor(
                 sensor.getClass(), target.getClass())) {
@@ -36,6 +38,13 @@ public class CookieCutterMediator extends SimEntityBase implements SensorTargetM
         }
     }
     
+    /** When the range is exited, what happens in general depends on
+     * the undetection rule.  In this case, simply schedule an undetection.
+     * Like the Detection event, the Undetection event is scheduled directly
+     * on the sensor, for the same reason.
+     * @param sensor The sensor whose range was exited
+     * @param target The target that exited the sensor's range
+     */    
     public void doExitRange(Sensor sensor, Mover target) {
         if (this == SensorTargetMediatorFactory.getMediator(
                 sensor.getClass(), target.getClass())) {
@@ -44,6 +53,9 @@ public class CookieCutterMediator extends SimEntityBase implements SensorTargetM
         }
     }
     
+    /** Do nothing
+     * @param propertyChangeEvent The heard PropertyChangeEvent
+     */    
     public void propertyChange(java.beans.PropertyChangeEvent propertyChangeEvent) {
     }
     
