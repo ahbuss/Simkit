84f461716776761ff1f2ca6d89842e40509b8726
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-10-23 18:01:57 +0000

    Pools two RandomNumber instances using XOR

--- src/simkit/random/PooledXORGenerator.java
+++ src/simkit/random/PooledXORGenerator.java
@@ -1,82 +1,20 @@
 package simkit.random;
 
-/**
- *
- * @author  Arnold Buss
+/** Pools two <code>RandomNumber</code> instances by XOR-ing
+ * their generated long values.
+ * @author Arnold Buss
  */
-public class PooledXORGenerator implements Pooled {
+public class PooledXORGenerator extends PooledGeneratorBase {
 
-    public static final double MULTIPLICATIVE_FACTOR = 1.0 / (1 << 32);
-    
-    private RandomNumber first;
-    private RandomNumber second;
-    
     /** Creates a new instance of PooledXORGenerator */
     public PooledXORGenerator() {
     }
     
-    /** @return  The next Uniform(0, 1) random number
+    /** @return  The next long - XOR's the two <code>RandomNumber</code>
+     *  instances.
      */
-    public double draw() {
-        return drawLong() * MULTIPLICATIVE_FACTOR;
-    }
-    
     public long drawLong() {
         return first.drawLong() ^ second.drawLong();
     }
-    
-    public RandomNumber getFirst() {
-        return first;
-    }
-    
-    public RandomNumber getSecond() {
-        return second;
-    }
-    
-    /** @return  The current random number seed
-     */
-    public long getSeed() {
-        throw new IllegalArgumentException("Get Seeds from first or second RandomNumber");
-    }
-    
-    /** @return  The current array of random number seed s
-     */
-    public long[] getSeeds() {
-        throw new IllegalArgumentException("Get Seeds from first or second RandomNumber");
-    }
-    
-    /**  Resets seed to last setSeed() value
-     */
-    public void resetSeed() {
-        first.resetSeed();
-        second.resetSeed();
-    }
-    
-    public void setFirst(RandomNumber first) {
-        this.first = first;
-    }
-    
-    public void setSecond(RandomNumber first) {
-        this.second = second;
-    }
-    
-    /** @param seed The new random number seed
-     */
-    public void setSeed(long seed) {
-        throw new IllegalArgumentException("Set Seeds in first or second RandomNumber");
-    }
-    
-    /** @param seed The new array of seeds
-     */
-    public void setSeeds(long[] seed) {
-        throw new IllegalArgumentException("Set Seeds in first or second RandomNumber");
-    }
-    
-    public Object clone() {
-        PooledXORGenerator copy = new PooledXORGenerator();
-        copy.setFirst((RandomNumber) first.clone());
-        copy.setSecond((RandomNumber) second.clone());
-        return copy;
-    }
-    
+
 }
