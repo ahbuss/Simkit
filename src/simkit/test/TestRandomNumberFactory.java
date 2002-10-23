309a156fec64fdac473d8145508954fb82fb8ea9
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2002-10-23 18:03:57 +0000

    For testing

---
+++ src/simkit/test/TestRandomNumberFactory.java
@@ -0,0 +1,46 @@
+package simkit.test;
+import simkit.random.*;
+/**
+ *
+ * @author  Arnold Buss
+ */
+public class TestRandomNumberFactory {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Class theClass = RandomNumberFactory.getClassFor("Congruential");
+        System.out.println(theClass);        
+        System.out.println(RandomNumberFactory.getCache());
+        theClass = RandomNumberFactory.getClassFor("simkit.random.PooledGenerator");
+        System.out.println(theClass);
+        System.out.println(RandomNumberFactory.getCache());
+        theClass = RandomNumberFactory.getClassFor("simkit.test.DummyGenerator");
+        System.out.println(theClass);
+        System.out.println(RandomNumberFactory.getCache());
+        RandomNumberFactory.addSearchPackage("simkit.test");
+        theClass = RandomNumberFactory.getClassFor("DummyGenerator");
+        System.out.println(theClass);
+        System.out.println(RandomNumberFactory.getCache());
+        
+        theClass = RandomNumberFactory.getClassFor("ExponentialVariate");
+        System.out.println(theClass);
+        theClass = RandomNumberFactory.getClassFor("TestRandomNumberFactory");
+        System.out.println(theClass);
+        
+        RandomNumberFactory.setDefaultClass("Tausworthe");
+        RandomNumber rng1 = RandomNumberFactory.getInstance(CongruentialSeeds.SEED[0]);
+        System.out.println(rng1);
+        
+        RandomNumber rng2 = RandomNumberFactory.getInstance("DummyGenerator");
+        System.out.println(rng2);
+        
+        rng2 = RandomNumberFactory.getInstance("Congruential");
+        
+        RandomNumber rng3 = RandomNumberFactory.getInstance("PooledXORGenerator",
+            rng1, rng2);
+        System.out.println(rng3);
+    }
+    
+}
