41c54dc43a26879084cacfb762231e7d0c47217e
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-04-03 23:12:51 +0000

    Moved old Server to Server2; this one now uses reflection, the "traditional" way.

--- src/simkit/examples/Server.java
+++ src/simkit/examples/Server.java
@@ -1,155 +1,89 @@
 package simkit.examples;
+
 import simkit.*;
 import simkit.random.*;
 
 /**
- * Multiple server queue using BasicSimEntity as superclass.  This approach
- * does not use reflection, but implicitly relies on the moral equivalent of a "switch"
- * statement to decide which event to execute when it is called back from Schedule (the
- * handleSimEvent() method).
- * @author Arnold Buss
+ *
+ * @author  Arnold Buss
  */
-public class Server extends BasicSimEntity {
-
-    private RandomVariate serviceTime;
+public class Server extends SimEntityBase {
+    
     private int totalNumberServers;
-
+    private RandomVariate serviceTime;
+    
     protected int numberAvailableServers;
     protected int numberInQueue;
     protected int numberServed;
-
-    public Server(int numServ, RandomVariate rv) {
-        serviceTime = RandomVariateFactory.getInstance(rv);
-        totalNumberServers = numServ;
+    
+    /** Creates a new instance of Simkit */
+    public Server(int servers, RandomVariate time) {
+        totalNumberServers = servers;
+        serviceTime = time;
     }
-
-    /** Resets state to empty and idle with no customers processed.
-     */    
+    
     public void reset() {
         super.reset();
-        numberAvailableServers = totalNumberServers;
         numberInQueue = 0;
+        numberAvailableServers = totalNumberServers;
         numberServed = 0;
     }
-
-    /** Simply fires the PropertyChangeEvents for time-varying state variables.
-     */    
+    
     public void doRun() {
         firePropertyChange("numberInQueue", numberInQueue);
         firePropertyChange("numberAvailableServers", numberAvailableServers);
     }
-
-    /** Arrivale of a customer to queue.  After incrementing the number in queue, 
-     *  if a server is available, a StartService event is scheduled.
-     */    
+    
     public void doArrival() {
         firePropertyChange("numberInQueue", numberInQueue, ++numberInQueue);
-
-        if (numberAvailableServers > 0) {
+        if (getNumberAvailableServers() > 0) {
             waitDelay("StartService", 0.0);
         }
     }
-
-    /** Decrement number in queue and number of available servers.
-     * Schedule EndService event after delay of a service time.
-     */    
+    
     public void doStartService() {
         firePropertyChange("numberInQueue", numberInQueue, --numberInQueue);
         firePropertyChange("numberAvailableServers", numberAvailableServers, --numberAvailableServers);
-
+        
         waitDelay("EndService", serviceTime.generate());
+        
     }
-
-    /** Increment number of available servers.  If a customer is
-     * waiting in the queue, schedule a StartService event immediately.
-     */    
+    
     public void doEndService() {
         firePropertyChange("numberAvailableServers", numberAvailableServers, ++numberAvailableServers);
-        firePropertyChange("numberServed", 0, ++numberServed);
-
-        if (numberInQueue > 0 ) {
+        firePropertyChange("numberServed", numberServed, ++numberServed);
+        if (getNumberInQueue() > 0) {
             waitDelay("StartService", 0.0);
         }
     }
-
-    /**
-     * @return Total number of servers (parameter)
-     */    
-    public int getTotalNumberServers() { return totalNumberServers; }
-
-    /**
-     * @return current number in queue (state)
-     */    
-    public int getNumberInQueue() { return numberInQueue; }
-
-    /**
-     * @return Current number of available servers (state)
-     */    
+    
     public int getNumberAvailableServers() { return numberAvailableServers; }
-
-    /**
-     * @return Total number of customers who have completed service (state)
-     */    
+    
+    public int getNumberInQueue() { return numberInQueue; }
+    
+    public int getNumberInSystem() {
+        return getNumberInQueue() + getNumberServers() - getNumberAvailableServers();
+    }
+    
+    public void setNumberServers(int servers) { 
+        if (totalNumberServers <= 0) {
+            throw new IllegalArgumentException("Need positive number of servers: " + servers);
+        }
+        totalNumberServers = servers; 
+    }
+    
+    public int getNumberServers() { return totalNumberServers; }
+    
+    public void setServiceTime(RandomVariate service) { serviceTime = service; }
+    
+    public RandomVariate getServiceTime() { return serviceTime; }
+    
     public int getNumberServed() { return numberServed; }
-
-    /**
-     * @return Service time RandomVariate
-     */    
-    public RandomVariate getServiceTime() { return RandomVariateFactory.getInstance(serviceTime); }
     
-    /**
-     * @param st  RandomVariate instance to generate service times from.
-     */    
-    public void setServiceTime(RandomVariate st) {serviceTime = st;}
-
-    public String paramString() {
-        return toString();
+    public String toString() { 
+        return "Multiple Server Queue\n\tNumber Servers:\t" + getNumberServers() +
+            "\n\tService Time Distribution:\t" + getServiceTime();
     }
     
-    /**
-     * @return Short description of class
-     */    
-    public String toString() {
-        return "Multiple Server Queue with " + totalNumberServers + " servers" +
-            "\nService time distribution is " + serviceTime;
-    }/**
- *  @return A short description of this class
-**/
-    public static String description() {
-        return "Model of Multiple Server Queue";
-    }
-    
-    /**
-     * Typically an Event is handled (as opposed to processed, as in SimEventListener)
-     * by actually executing a method.
-     * @param event The SimEvent to be handled.
-     */
-    public void handleSimEvent(SimEvent event) {
-        String thisEvent = event.getEventName();
-        if (thisEvent.equals("Run")) {
-            doRun();
-        }
-        
-        else if(thisEvent.equals("Arrival")) {
-            doArrival();
-        }
-        
-        else if(thisEvent.equals("StartService")) {
-            doStartService();
-        }
-        else if (thisEvent.equals("EndService")) {
-            doEndService();
-        }
-    }
-    
-    /** Callback method when a SimEvent is "heard" via the SimEventListener pattern
-     * @param event "Listened-to" event.
-     */    
-    public void processSimEvent(SimEvent event) {
-        String thisEvent = event.getEventName();
-        if (thisEvent.equals("Arrival") || thisEvent.equals("EndService")) {
-            waitDelay("Arrival", 0.0);;
-        }
-    }
-    
+    public String paramString() { return toString(); }
 }
