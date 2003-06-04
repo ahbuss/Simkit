69fdd27fcf9f4e5aeafe6a62afb823667aed68a4
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-06-04 21:59:20 +0000

    Can specify the min value, max value, and # cells

--- src/simkit/stat/Histogram.java
+++ src/simkit/stat/Histogram.java
@@ -2,27 +2,38 @@ package simkit.stat;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
+import java.text.DecimalFormat;
 
-public class Histogram implements PropertyChangeListener {
+public class Histogram extends SimpleStatsTally {
 
-    private String name;
     private int[] frequency;
     private int total;
+    private double lowValue;
+    private double cellWidth;
 
     public Histogram(String name, int number) {
-        this.name = name;
+        this(name, 0, (double) number, number);
+    }
+
+    public Histogram(String name, double lowVal, double hiVal, int number) {
+        super(name);
         frequency = new int[number + 1];
+        lowValue = lowVal;
+        cellWidth = (hiVal - lowVal) /  number;
     }
-
-    public void propertyChange(PropertyChangeEvent e) {
-        if (e.getPropertyName().equals(name) ){
-            frequency[ (Math.min(((Number) e.getNewValue()).intValue(), frequency.length - 1))]++;
-            total++;
-        }
+    
+    public void newObservation(double x) {
+        super.newObservation(x);
+        int index = (int) Math.floor(( x - lowValue) / cellWidth);
+        index = Math.min( Math.max(index, 0), frequency.length - 1);
+        frequency[index]++;
+        total++;
     }
-
+    
     public void reset() {
-        frequency = new int[frequency.length];
+        if (frequency != null) {
+            frequency = new int[frequency.length];
+        }
         total = 0;
     }
 
@@ -37,10 +48,14 @@ public class Histogram implements PropertyChangeListener {
     }
 
     public String toString() {
-        StringBuffer buf = new StringBuffer(name + "\nindex\tFrequencies\tProportions\n");
+        DecimalFormat form = new DecimalFormat("0.000");
+        StringBuffer buf = new StringBuffer(super.toString());
+        buf.append("\nvalue\tFrequencies\tProportions\n");
         double[] prop = getProportions();
         for (int i = 0; i < frequency.length; i++ ) {
-            buf.append(i);
+//            buf.append(i);
+//            buf.append('\t');
+            buf.append(form.format(lowValue + cellWidth * i));
             buf.append('\t');
             buf.append(frequency[i]);
             buf.append('\t');
@@ -48,7 +63,6 @@ public class Histogram implements PropertyChangeListener {
             buf.append(SampleStatistics.EOL);
         }
         return buf.toString();
-
     }
 
 }
