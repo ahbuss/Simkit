5f1b15fa6a9a82ec7f26b1caf27a4acb0ac2bae8
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-07-07 02:52:21 +0000

    Fixed build files

--- buildoc.bat
+++ buildoc.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist simsystem mkdir simsystem
 if not exist simsystem\doc mkdir simsystem\doc
-cd ..
+
 javadoc -author -private -d simsystem\doc simkit simkit.data simkit.smd simkit.stat simkit.random simkit.util simkit.examples simkit.smdx
-pause
+
--- buildsrc.bat
+++ buildsrc.bat
@@ -1,3 +1,3 @@
 @echo off
 if not exist simsystem mkdir simsystem
-jar cvfM simsystem\simkit-src.zip graph simkit *.bat *.sh *.txt
+jar cvfM simsystem\simkit-src.zip graph\*.java simkit\*.java simkit\*.txt simkit\data\*.java simkit\random\*.java simkit\stat\*.java simkit\smd\*.java simkit\smdx\*.java simkit\examples\*.java *.bat *.sh *.txt
