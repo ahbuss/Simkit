4c7b69e6ad87431b1e50ff6405a73bbd5a645a37
abuss <abuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-09-17 19:35:57 +0000

    removed deprecated simkit.data and simkit.smd

--- buildoc.bat
+++ buildoc.bat
@@ -2,5 +2,5 @@
 if not exist simsystem mkdir simsystem
 if not exist simsystem\doc mkdir simsystem\doc
 
-javadoc -author -private -d simsystem\doc simkit simkit.data simkit.smd simkit.stat simkit.random simkit.util simkit.examples simkit.smdx
+javadoc -author -private -d simsystem\doc simkit simkit.stat simkit.random simkit.util simkit.examples simkit.smdx
 
