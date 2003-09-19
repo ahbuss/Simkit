2fff2c06a3513d2234ea396c71b4422acca734d7
jlruck <jlruck@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-09-19 21:38:33 +0000

    Added public javadoc

--- makejavadoc.bat
+++ makejavadoc.bat
@@ -1,5 +1,6 @@
-@rem Create private javadoc for simkit and graph. Errors and Warnings go to stdout.
+@rem Create private and public javadoc for simkit and graph. Errors and Warnings go to stdout.
 @rem John Ruck, Rolands and Associates Corporation
 @rem 9/17/03
+javadoc -public -d c:\simkit_javadoc\public -sourcepath c:\tmp\simkit -subpackages simkit:graph -classpath c:\tmp\simkit\lib\jdom.jar -breakiterator > c:\simkit_javadoc\output.txt
 javadoc -private -d c:\simkit_javadoc\private -sourcepath c:\tmp\simkit -subpackages simkit:graph -classpath c:\tmp\simkit\lib\jdom.jar -breakiterator > c:\simkit_javadoc\output.txt
 
