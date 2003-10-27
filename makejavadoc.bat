0e4bbd4b8d71875da4386f9336de49c0b4251d66
jlruck <jlruck@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-10-27 17:16:36 +0000

    Removed graph, added version and Author flags

--- makejavadoc.bat
+++ makejavadoc.bat
@@ -1,6 +1,11 @@
-@rem Create private and public javadoc for simkit and graph. Errors and Warnings go to stdout.
+@rem Create private and public javadoc for simkit. Errors and Warnings go to stdout.
 @rem John Ruck, Rolands and Associates Corporation
 @rem 9/17/03
-javadoc -public -d c:\simkit_javadoc\public -sourcepath c:\tmp\simkit -subpackages simkit:graph -classpath c:\tmp\simkit\lib\jdom.jar -breakiterator -link http://java.sun.com/j2se/1.4/docs/api/ > c:\simkit_javadoc\output.txt
-javadoc -private -d c:\simkit_javadoc\private -sourcepath c:\tmp\simkit -subpackages simkit:graph -classpath c:\tmp\simkit\lib\jdom.jar -breakiterator -link http://java.sun.com/j2se/1.4/docs/api/ > c:\simkit_javadoc\output.txt
+@rem The Header: $Header$
+@rem The log: $Log$
+@rem The log: Revision 1.4  2003/10/27 17:16:36  jlruck
+@rem The log: Removed graph, added version and Author flags
+@rem The log:
+javadoc -public -d c:\simkit_javadoc\public -sourcepath c:\tmp\simkit -subpackages simkit -classpath c:\tmp\simkit\lib\jdom.jar -breakiterator -link http://java.sun.com/j2se/1.4/docs/api/ > c:\simkit_javadoc\output.txt
+javadoc -private -version -author -d c:\simkit_javadoc\private -sourcepath c:\tmp\simkit -subpackages simkit -classpath c:\tmp\simkit\lib\jdom.jar -breakiterator -link http://java.sun.com/j2se/1.4/docs/api/ > c:\simkit_javadoc\output.txt
 
