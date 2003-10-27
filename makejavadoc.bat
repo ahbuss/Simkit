033d4cacbe5387b0c3481288331899eb48cdaa2d
jlruck <jlruck@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-10-27 22:13:59 +0000

    Removed the Log keyword from the comments

--- makejavadoc.bat
+++ makejavadoc.bat
@@ -1,11 +1,7 @@
 @rem Create private and public javadoc for simkit. Errors and Warnings go to stdout.
 @rem John Ruck, Rolands and Associates Corporation
 @rem 9/17/03
-@rem The Header: $Header$
-@rem The log: $Log$
-@rem The log: Revision 1.4  2003/10/27 17:16:36  jlruck
-@rem The log: Removed graph, added version and Author flags
-@rem The log:
+@rem $Id$
 javadoc -public -d c:\simkit_javadoc\public -sourcepath c:\tmp\simkit -subpackages simkit -classpath c:\tmp\simkit\lib\jdom.jar -breakiterator -link http://java.sun.com/j2se/1.4/docs/api/ > c:\simkit_javadoc\output.txt
 javadoc -private -version -author -d c:\simkit_javadoc\private -sourcepath c:\tmp\simkit -subpackages simkit -classpath c:\tmp\simkit\lib\jdom.jar -breakiterator -link http://java.sun.com/j2se/1.4/docs/api/ > c:\simkit_javadoc\output.txt
 
