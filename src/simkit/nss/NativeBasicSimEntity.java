c5205bdbc18a76629e89146d7f7978682126010f
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-11-05 23:02:45 +0000

    First attempt.

---
+++ src/simkit/nss/NativeBasicSimEntity.java
@@ -0,0 +1,64 @@
+package simkit.nss;
+
+import simkit.BasicSimEntity;
+import simkit.SimEvent;
+
+/**
+ * <p>An attmept to use NSS Objects driven by the Simkit engine.
+ * The idea is to use native methods to do the callbacks for 
+ * processSimEvent() and handleSimEvent().  Since the state will typically
+ * be maintained outside Simkit, a native run() method is called for
+ * in order to perform whatever initializations are required.
+ *
+ * <p>The header file simkit_nss_NativeBasicSimEntity.h can be generated
+ * by the call
+ * <pre>
+ *    > javah simkit.nss.NativeBasicSimEntity
+ * </pre>
+ * from the command line, where the current working directory is one level 
+ * above the simkit folder.
+ *
+ * @author  Arnold Buss
+ */
+public class NativeBasicSimEntity extends BasicSimEntity {
+    
+    public NativeBasicSimEntity() {
+        super();
+    }
+    
+    /**
+     * @param name The name of the NativeBasicSimEntity created
+     */    
+    public NativeBasicSimEntity(String name) {
+        super(name);
+    }
+    
+    /**
+     * @param priority The priority of the NativeBasicSimEntity instance
+     */    
+    public NativeBasicSimEntity(double priority) {
+        super(priority);
+    }
+    
+    /**
+     * @param name The name of the NativeBasicSimEntity
+     * @param priority The priority of the NativeBasicSimEntity
+     */    
+    public NativeBasicSimEntity(String name, double priority) {
+        super(name, priority);
+    }
+    
+    /** Perform whatever initializations are required. */    
+    public native void reset();
+    
+    /** This method is called from Schedule and will only
+     * be passed SimEvents it had originally scheduled.
+     * @param simEvent The SimEvent instance.
+     */    
+    public native void handleSimEvent(SimEvent simEvent);
+    
+    /** This callback is received by listening to another SimEntity
+     * @param simEvent The SimEvent being passed
+     */    
+    public native void processSimEvent(SimEvent simEvent);
+}
