fb8d3e97f090c1aff5ba14f677a116051b303d34
jlruck <jlruck@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-11-18 23:43:54 +0000

    Added additional parameter checks.

--- src/simkit/random/TwoStateMarkovVariate.java
+++ src/simkit/random/TwoStateMarkovVariate.java
@@ -13,22 +13,39 @@ package simkit.random;
  */
 public class TwoStateMarkovVariate extends RandomVariateBase implements DiscreteRandomVariate {
     
+/**
+* A two element array holding the transition probabilities.
+**/
     private double[] transitionProb;
+
+/**
+* The state of the system last time it was set.
+**/
     private int initialState;
+
+/**
+* The current state of the system 
+**/
     protected int currentState;
     
+/**
+* Makes a new TwoStateMarkovVariate with initialState 0 and no transition probabilities.
+* The transition probabilities must be set prior to use.
+**/
     public TwoStateMarkovVariate() {
     }
     
-    /** Generate a random variate having this class's distribution.
-     * @return The generated random variate
+    /** 
+     * Generate the next state of the system 
+     * @return The state of the system (0 or 1) cast to a double 
      */
     public double generate() {
         return (double) generateInt();
     }
     
     /**
-     * @return generated value
+     * Generate the next state of the system
+     * @return The state of the system (0 or 1).
      */    
     public int generateInt() {
         int state = currentState;
@@ -36,28 +53,48 @@ public class TwoStateMarkovVariate extends RandomVariateBase implements Discrete
         return state;
     }
     
-    /** Returns the array of parameters as an Object[].
-     * @return the array of parameters as an Object[].
-     */
+/**
+* Returns a 2 element array with the parameters. The first element is copy of the transition
+* probability array. The second element is the initial state as an Integer.
+**/
     public Object[] getParameters() {
         return new Object[] { transitionProb.clone(), new Integer(initialState) };
     }
     
-    /**  Sets the random variate's parameters.
-     *  Alternatively, the parameters could be set in the constructor or
-     *  in additional methods provided by the programmer.
-     * @param params the array of parameters, wrapped in objects.
-     */
+/**
+* Sets the transition probabilities and (optionally) the initial state.
+* @param params A 1 or 2 element array. The first element contains a two element
+* <code>double</code> array with the probability that the state of the system will
+* stay 0 given that it is 0 and the probability that the system will stay 1 given that it is 1.
+* The second (optional) element is the initial state (0 or 1) of the system as a Number.
+* @throws IllegalArgumentException If the array does not contain 1 or 2 elements,
+* If the first element is not a 2 element <code>double</code> array with values
+* between 0 and 1 inclusive, or if the 2nd (optional) element is not a Number
+* with a value of 0 or 1.
+*
+**/
     public void setParameters(Object[] params) {
+        if (params.length != 1 && params.length != 2) {
+            throw new IllegalArgumentException("A 1 or 2 element array is required.");
+        }
         if (params.length == 1 && params[0] instanceof double[]) {
             setTransitionProb((double[]) params[0]);
         }
         else if (params.length == 2 && params[0] instanceof double[] && params[1] instanceof Number) {
             setTransitionProb((double[]) params[0]);
             setInitialState(((Number) params[1]).intValue());
+        } else {
+            throw new IllegalArgumentException("Either the first element is not a double[] or the second is not a Number");
         }
     }
     
+/**
+* Sets the transition probabilities.
+* @param trans A two element array with the probability that the state of the system will
+* stay 0 given that it is 0 and the probability that the system will stay 1 given that it is 1.
+* @throws IllegalArgumentException If the array does not contain 2 elements or either element
+* is not between 0 and 1 inclusive.
+**/
     public void setTransitionProb (double[] trans) {
         if (trans.length == 2) {
             if (trans[0] >= 0.0 && trans[0] <= 1.0 && trans[1] >= 0.0 && trans[1] <= 1.0) {
@@ -69,17 +106,22 @@ public class TwoStateMarkovVariate extends RandomVariateBase implements Discrete
             }
         }
         else {
-            throw new IllegalArgumentException("Need 2 probabilities: " + trans.length);
+            throw new IllegalArgumentException("Need 2 probabilities: " + trans.length + " given");
         }
     }
     
     /**
+     * Returns a 2 element array containing the transition probabilities.
      * @return probabilities of not changing state in 2-d array
      */    
     public double[] getTransitionProb() {
         return (double[]) transitionProb.clone();
     }
     
+/**
+* Sets the initial state (0 or 1) of the system
+* @throws IllegalArgumentException If the value is not 0 or 1.
+**/
     public void setInitialState(int state) {
         if (state == 0 || state == 1) {
             initialState = state;
@@ -90,8 +132,15 @@ public class TwoStateMarkovVariate extends RandomVariateBase implements Discrete
         }
     }
     
+/**
+* Returns the initial state of the system.
+**/
     public int getInitialState() { return initialState; }
     
+/**
+* Returns a String containing the name of this RandomVariate, the transition
+* probabilities and the initial state.
+**/
     public String toString() {
         return "Two State Markov Chain [" + getTransitionProb()[0] + ", " +
             getTransitionProb()[1] + "] - {" + getInitialState() +"}";
