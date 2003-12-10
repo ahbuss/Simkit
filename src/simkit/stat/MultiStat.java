f34018088109b8e2abe0fb353bce26beb93578f5
jlruck <jlruck@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-12-10 18:21:23 +0000

    Changed getTallyStats to return null rather than throw nullPointerException if property not found. Added getTimeVaryingStats.

--- src/simkit/stat/MultiStat.java
+++ src/simkit/stat/MultiStat.java
@@ -8,7 +8,7 @@ import java.util.*;
 /**
  * Listens for PropertyChangeEvents that are associated with state changes
  * to be collected in a Tally manner.  A new SimpleStatsTally and a
- * new SimpleStatsTimeVarying will be created on-the-fly if necessary.
+ * new SimpleStatsTimeVarying will be created on-the-fly for each unique property name.
  * Thus, the user will have a choice as to whether to see Tally or
  * Time-Varying statistics.  Typically, of course, only one will
  * make any sense.
@@ -18,19 +18,37 @@ import java.util.*;
  */
 public class MultiStat implements PropertyChangeListener {
     
+/**
+* A container for the SimpleStatsTallies.
+**/
     private Map tallyStats;
+
+/**
+* A container for the SimpleStatsTimeVarying.
+**/
     private Map timeVaryingStats;
     
+/**
+* Creates a new empty MultiStat.
+**/
     public MultiStat() {
         tallyStats = new TreeMap();
         timeVaryingStats = new TreeMap();
     }
     
+/**
+* Resets this MultiStat to its original empty state.
+**/
     public void reset() {
         tallyStats.clear();
         timeVaryingStats.clear();
     }
     
+/**
+* Notifies this MultiStat of a property change. Adds the observation to 
+* both a SimpleStatsTally and a SimpleStatsTimeVarying, creating them if
+* this is the first observation for the given property.
+**/
     public void propertyChange(PropertyChangeEvent evt) {
         Object value = evt.getNewValue();
         if (value instanceof Number) {
@@ -46,6 +64,11 @@ public class MultiStat implements PropertyChangeListener {
         }
     }
     
+/**
+* Adds the observation to 
+* both a SimpleStatsTally and a SimpleStatsTimeVarying, creating them if
+* this is the first observation for the given property.
+**/
     protected void newObservation(String name, double value) {
         SimpleStatsTally sst = (SimpleStatsTally) tallyStats.get(name);
         if (sst == null) {
@@ -59,27 +82,51 @@ public class MultiStat implements PropertyChangeListener {
         sstv.newObservation(value);
     }
     
+/**
+* Adds the observation to 
+* both a SimpleStatsTally and a SimpleStatsTimeVarying, creating them if
+* this is the first observation for the given property.
+**/
     protected void newObservation(String name, boolean value) {
         newObservation(name, value ? 1.0 : 0.0);
     }
     
+/**
+* Creates a new SimpleStatsTally for the given property name and adds it to 
+* the Map.
+**/
     protected SimpleStatsTally createNewTallyStat(String name) {
         SimpleStatsTally newSST = new SimpleStatsTally(name);
         tallyStats.put(name, newSST);
         return newSST;
     }
     
+/**
+* Creates a new SimpleStatsTimeVarying for the given property name and adds it to 
+* the Map.
+**/
     protected SimpleStatsTimeVarying createNewTimeVaryingStat(String name) {
         SimpleStatsTimeVarying newSSTV = new SimpleStatsTimeVarying(name);
         timeVaryingStats.put(name, newSSTV);
         return newSSTV;
     }
     
+/**
+* Returns a copy of the SimpleStatsTally for the given property name. Returns
+* <code>null<code/> if the property cannot be found.
+**/
     public SimpleStatsTally getTallyStats(String name) {
         SimpleStatsTally sst = (SimpleStatsTally) tallyStats.get(name);
-        return (SimpleStatsTally) sst.clone();
+        if (sst == null) {
+           return null;
+        } else {
+           return (SimpleStatsTally) sst.clone();
+        }
     }
     
+/**
+* Returns an array containing a copy of all of the SimpleStatsTallies.
+**/
     public SimpleStatsTally[] getAllTallyStats() {
         SimpleStatsTally[] sst = (SimpleStatsTally[]) tallyStats.values().toArray(new SimpleStatsTally[0]);
         for (int i = 0; i < sst.length; ++i) {
@@ -88,6 +135,21 @@ public class MultiStat implements PropertyChangeListener {
         return sst;
     }
     
+/**
+* Returns a copy of the SimpleStatsTimeVarying for the given property name.
+* Returns <code>null<code/> if the property cannot be found.
+**/
+    public SimpleStatsTimeVarying getTimeVaryingStats(String name) {
+        SimpleStatsTimeVarying sst = (SimpleStatsTimeVarying) timeVaryingStats.get(name);
+        if (sst == null) {
+           return null;
+        } else {
+           return (SimpleStatsTimeVarying) sst.clone();
+        }
+    }
+/**
+* Returns an array containing a copy of all of the SimpleStatsTimeVarying.
+**/
     public SimpleStatsTimeVarying[] getAllTimeVaryingStats() {
         SimpleStatsTimeVarying[] sstv = 
             (SimpleStatsTimeVarying[]) timeVaryingStats.values().toArray(new SimpleStatsTimeVarying[0]);
@@ -97,10 +159,17 @@ public class MultiStat implements PropertyChangeListener {
         return sstv;
     }
     
+/**
+* Returns an array containing the names of all of the properties for which
+* this MultiStat has collected data.
+**/
     public String[] getNames() {
         return (String[]) tallyStats.keySet().toArray(new String[0]);
     }
     
+/**
+* Returns a String containing information about the data contained in this MultiStat.
+**/
     public String toString() {
         StringBuffer buf = new StringBuffer("MultiStatTally");
         buf.append(SimEntity.NL);
