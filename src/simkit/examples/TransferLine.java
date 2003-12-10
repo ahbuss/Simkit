d5dc4074f1eeed0a5e8e883ab9d28c5f912646af
jlruck <jlruck@c6e24747-a972-dc11-9d74-0011435b89d6>
2003-12-10 00:48:23 +0000

    Added a property change for numberServed to the EndService event for the last station

--- src/simkit/examples/TransferLine.java
+++ src/simkit/examples/TransferLine.java
@@ -4,31 +4,79 @@ import simkit.SimEntityBase;
 import simkit.random.RandomVariate;
 import simkit.random.RandomVariateFactory;
 
+/**
+* Implements a transfer line with a user determined number of stations and a user
+* determined number of servers at each station.
+* @version $Id$
+*/
 public class TransferLine extends SimEntityBase {
 
+/**
+* The RandomVariate used to generate the interarrival times.
+**/
     private RandomVariate interarrivalTime;
+
+/**
+* The RandomVariates used to generate the service times for servers at each
+* station.
+**/
     private RandomVariate[] serviceTime;
+
+/**
+* The number of servers at each station.
+**/
     private int[] numberServersAtStation;
 
+/**
+* The length of the queue at each station.
+**/
     protected int[] numberInQueue;
+
+/**
+* The number of servers that are not busy at each station.
+**/
     protected int[] numberAvailableServers;
+
+/**
+* The total number of arrivals into the system.
+**/
     protected int numberArrivals;
+
+/**
+* The total number served by the system. (The number servered by the
+* last station.)
+**/
     protected int numberServed;
 
-    public TransferLine(RandomVariate iat, int[] ns, RandomVariate[] st) {
-        if (ns.length != st.length) {
+/**
+* Constructs a new TransferLine. The number of stations is determined by the
+* length of numberOfServers and serviceTime, which must be the same.
+* Note: Copies are made of the RandomVariates.
+* @param arrivalTime The RandomVariate used to generate interarrival times.
+* @param numberOfServers The number of servers at each station.
+* @param serviceTime The RandomVariates used to generate the service times at
+* each station.
+* @throws IllegalArgumentException If the two arrays are not the same length.
+**/
+    public TransferLine(RandomVariate arrivalTime, 
+                        int[] numberOfServers, 
+                        RandomVariate[] serviceTime) {
+        if (numberOfServers.length != serviceTime.length) {
             throw new IllegalArgumentException("Different number of stations "+
-                        "defined in # servers (" + ns.length + ") than "+
-                        "service times (" + st.length + ")");
+                        "defined in # servers (" + numberOfServers.length + ") than "+
+                        "service times (" + serviceTime.length + ")");
         }
-        numberServersAtStation = (int[]) ns.clone();
-        interarrivalTime = RandomVariateFactory.getInstance(iat);
-        serviceTime = new RandomVariate[st.length];
-        for (int i = 0; i < st.length; i++) {
-            serviceTime[i] = RandomVariateFactory.getInstance(st[i]);
+        numberServersAtStation = (int[]) numberOfServers.clone();
+        interarrivalTime = RandomVariateFactory.getInstance(arrivalTime);
+        this.serviceTime = new RandomVariate[serviceTime.length];
+        for (int i = 0; i < serviceTime.length; i++) {
+            this.serviceTime[i] = RandomVariateFactory.getInstance(serviceTime[i]);
         }
     }
 
+/**
+* Resets the system to its initial state.
+**/
     public void reset() {
         super.reset();
         numberInQueue = new int[numberServersAtStation.length];
@@ -37,6 +85,10 @@ public class TransferLine extends SimEntityBase {
         numberServed = 0;
     }
 
+/**
+* Schedules first Arrival event. Fires property changes for numberInQueue and
+* numberAvailableServers.
+**/
     public void doRun() {
         for (int i = 0; i < numberInQueue.length; i++) {
             fireIndexedPropertyChange(i, "numberInQueue", numberInQueue[i]);
@@ -46,12 +98,20 @@ public class TransferLine extends SimEntityBase {
         waitDelay("Arrival", interarrivalTime.generate());
     }
 
+/**
+* An arrival into the system. Schedules the next arrival into the system. Schedules
+* an Arrival for now to the first station. Fires a property change for numberArrivals.
+**/
     public void doArrival() {
         firePropertyChange("numberArrivals", ++numberArrivals);
         waitDelay("Arrival", 0.0,  new Object[] { new Integer(0) } );
         waitDelay("Arrival", interarrivalTime.generate());
     }
 
+/**
+* An arrival at the given station. If a server is available at this station, schedules
+* an StartService for now. Fires a property change for numberInQueue.
+**/
     public void doArrival(int station) {
         fireIndexedPropertyChange(station, "numberInQueue", numberInQueue[station],
             ++numberInQueue[station]);
@@ -60,6 +120,10 @@ public class TransferLine extends SimEntityBase {
         }
     }
 
+/**
+* StartService at the given station. Schedules EndService at this station. Fires
+* property changes for the numberInQueue and numberAvailableServers at this station.
+**/
     public void doStartService(int station) {
         fireIndexedPropertyChange(station, "numberInQueue", numberInQueue[station],
             --numberInQueue[station]);
@@ -68,6 +132,12 @@ public class TransferLine extends SimEntityBase {
         waitDelay("EndService", serviceTime[station].generate(), new Object[] { new Integer(station) });
     }
 
+/**
+* EndService event at the given station. If this station's queue is not empty, schedule
+* an Arrival at this station for now. If this is not the last station, schedule an Arrival
+* at the next station for now. Fire property change for numberAvailableServers for this
+* station. If this is the last station, fire a property change for numberServed.
+**/
     public void doEndService(int station) {
         fireIndexedPropertyChange(station, "numberAvailableServers", numberAvailableServers[station],
             ++numberAvailableServers[station]);
@@ -78,9 +148,14 @@ public class TransferLine extends SimEntityBase {
 
         if (station < numberServersAtStation.length - 1) {
             waitDelay("Arrival", 0.0, new Object[] { new Integer(station + 1) });
+        } else {
+            firePropertyChange("numberServed", ++numberServed);
         }
     }
 
+/**
+* Returns a String with information about this TransferLine.
+**/
     public String paramString() {
         StringBuffer buf = new StringBuffer("Transfer Line with ");
         buf.append(numberServersAtStation.length);
