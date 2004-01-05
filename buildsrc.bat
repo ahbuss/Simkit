3a940cddf179f9a87c8b280aef82dc105cf586df
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-01-05 17:48:36 +0000

    Had been omitting simkit.util - fixed.

--- buildsrc.bat
+++ buildsrc.bat
@@ -1,3 +1,3 @@
 @echo off
 if not exist simsystem mkdir simsystem
-jar cvfM simsystem\simkit-src.zip graph\*.java simkit\*.java simkit\*.txt simkit\random\*.java simkit\stat\*.java simkit\smdx\*.java simkit\examples\*.java *.bat *.sh *.txt
+jar cvfM simsystem\simkit-src.zip graph\*.java simkit\*.java simkit\*.txt simkit\examples\*.java simkit\random\*.java simkit\smdx\*.java simkit\stat\*.java simkit\test\*.java simkit\util\*.java simkit\xml\*.java *.bat *.sh *.txt
