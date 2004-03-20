cc1cc9723e2549977cb71d3ad41a78f1746a0a88
jmbailey <jmbailey@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-03-20 00:13:30 +0000

    New Edge component class

---
+++ src/simkit/viskit/EdgeParameter.java
@@ -0,0 +1,19 @@
+package simkit.viskit;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mike
+ * Date: Mar 16, 2004
+ * Time: 3:02:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EdgeParameter
+{
+  public EdgeParameter(String name, Object type)
+  {
+    this.name = name;
+    this.type = type;
+  }
+  public String name;
+  public Object type;
+}
