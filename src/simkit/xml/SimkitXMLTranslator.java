6b22bd645985d37fb659259b55ac4ff4b8f9e931
rickg <rickg@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-03-23 23:18:24 +0000

    updated comment for usage

--- src/simkit/xml/SimkitXMLTranslator.java
+++ src/simkit/xml/SimkitXMLTranslator.java
@@ -19,7 +19,18 @@ public class SimkitXMLTranslator {
     
     /**
      * @param args the command line arguments
+     * The class reads XML documents according to specified DTD and
+     * translates all related events into SimkitHandler events.
+     * <p>Usage sample on command line:
+     * <pre>
+     *    java SimkitXMLTranslator mySimkitModule.xml
+     * </pre>
+     *
+     * To run from Netbeans IDE, select SimkitXMLTranslator.java
+     * from the Explorer panel, right click popup menu Properties,
+     * select Execution tab, enter name of file to parse in Arguments
      */
+    
     public static void main(String[] args) {
         SimkitHandler simkitHandler = new SimkitHandlerImpl();
         EntityResolver entityResolver = null;
