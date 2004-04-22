bae53a5e39b1f53ec018d4bb2413a779882137a9
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-04-22 02:41:57 +0000

    Files are in Viskit module now

--- src/simkit/xml/generate_bindings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# first source the SOURCE_ME environment before running this
-# also, you may need to edit the SOURCE_ME to fit your jaxb install
-# directory and jdk home
-
-xjc.sh -p simkit.xsd.bindings -extension simkit.xsd -d bindings
