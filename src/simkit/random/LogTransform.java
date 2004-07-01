81cbd7ebff5ae6c7d3edad3f3137a81ccee713bd
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-07-01 16:44:10 +0000

    Fixed in same way as ExponentialTransform Added test class

--- src/simkit/random/LogTransform.java
+++ src/simkit/random/LogTransform.java
@@ -1,88 +1,90 @@
 package simkit.random;
 
 /**
-* Generates random variates that are the log transform of another random variate.
-* The underlying RandomVariate is specified in the constructor and cannot be
-* changed. The <code>generate</code> method generates the underlying RandomVariate
-* and if a positive number returns the natural log of it, if negative or zero
-* it returns <code>Double.NaN</code>.
-*
-* @version $Id$
-**/
+ * Generates random variates that are the log transform of another random variate.
+ * The <code>generate</code> method generates the underlying RandomVariate
+ * and if a positive number returns the natural log of it, if negative or zero
+ * it returns <code>Double.NaN</code>.
+ *
+ * @version $Id$
+ **/
 public class LogTransform  implements RandomVariate {
-
-/**
-* The instance of the underlying RandomVariate.
-**/
-  private RandomVariate rv;
-
-/**
-* Constructs a new LogTransform based on the given RandomVariate with the given seed.
-**/
-  public LogTransform(RandomVariate rv, long seed) {
-    this(rv);
-    rv.getRandomNumber().setSeed(seed);
-  }
-
-/**
-* Constructs a new LogTransform based on the given RandomVariate with the default
-* seed(s).
-**/
-  public LogTransform(RandomVariate rv) {
-    this.rv = rv;
-  }
-
-/**
-* Generates the next LogTransform variate. If the underlying RandomVariate
-* generates zero or a negative number, returns <code>Double.NaN</code>.
-**/
-  public double generate() {
-    double original = rv.generate();
-    if (original > 0) {
-      return Math.log(original);
+    
+    /**
+     * The instance of the underlying RandomVariate.
+     **/
+    private RandomVariate rv;
+    
+    /**
+     * Constructs a new LogTransform.
+     **/
+    public LogTransform() {
     }
-    else {
-      return Double.NaN;
+    
+    /**
+     * Generates the next LogTransform variate. If the underlying RandomVariate
+     * generates zero or a negative number, returns <code>Double.NaN</code>.
+     **/
+    public double generate() {
+        double original = rv.generate();
+        if (original > 0) {
+            return Math.log(original);
+        }
+        else {
+            return Double.NaN;
+        }
     }
-  }
-
-/**
-* Sets the seed of the supporting RandomNumber of the underlying RandomVariate.
-**/
-  public void setSeed(long seed) {rv.getRandomNumber().setSeed(seed);}
-
-/**
-* Resets the seed of the supporting RandomNumber of the underlying RandomVariate
-* the the value last set.
-**/
-  public void resetSeed() {rv.getRandomNumber().resetSeed();}
-
-/**
-* Returns the current seed of the supporting RandomNumber of the underlying 
-* RandomVariate.
-**/
-  public long getSeed() { return rv.getRandomNumber().getSeed(); }
-
-/**
-* Returns an array containing the parameters of the underlying RandomVariate.
-**/
-  public Object[] getParameters() {return rv.getParameters();}
-
-/**
-* Sets the parameters of the underlying RandomVariate.
-**/
-  public void setParameters(Object[] params) {rv.setParameters(params);}
-
-/**
-* Returns the instance of RandomNumber that supports the underlying
-* RandomVariate.
-**/
-  public RandomNumber getRandomNumber() { return rv.getRandomNumber(); }
-
-/**
-* Sets the instance of RandomNumber that supports the underlying
-* RandomVariate.
-**/
-  public void setRandomNumber(RandomNumber rng) { rv.setRandomNumber(rng); }
-
+    
+    /**
+     * Returns an array containing the parameters of the underlying RandomVariate.
+     **/
+    public Object[] getParameters() {
+        return new Object[] { getRandomVariate() };
+    }
+    
+    /**
+     * Sets the parameters of the underlying RandomVariate.
+     **/
+    public void setParameters(Object[] params) {
+        if (params.length != 1) {
+            throw new IllegalArgumentException("Need 1 parameter: " + params.length);
+        }
+        if (! (params[0] instanceof simkit.random.RandomVariate) ) {
+            throw new IllegalArgumentException("RandomVariate instance needed: " + params[0].getClass().getName());
+        }
+        setRandomVariate( (RandomVariate) params[0]);
+    }
+    
+    /**
+     *
+     * @param randVar Underlying RandomVariate instance
+     */    
+    public void setRandomVariate(RandomVariate randVar) { rv = randVar; }
+    
+    /**
+     *
+     * @return Underlying RandomVariate instance
+     */    
+    public RandomVariate getRandomVariate() { return rv; }
+    
+    /**
+     * Returns the instance of RandomNumber that supports the underlying
+     * RandomVariate.
+     **/
+    public RandomNumber getRandomNumber() { return rv.getRandomNumber(); }
+    
+    /**
+     * Sets the instance of RandomNumber that supports the underlying
+     * RandomVariate.
+     **/
+    public void setRandomNumber(RandomNumber rng) { rv.setRandomNumber(rng); }
+    
+    /**
+     * Form is "Log { underlying RandomVariate toString() }"
+     * @return Description
+     */    
+    public String toString() {
+        return "Log { " + getRandomVariate() + " }";
+    }
+    
 }
