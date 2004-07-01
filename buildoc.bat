010907411b26870483c4b530efef7b85d7b9e861
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-07-01 00:02:13 +0000

    Changed to build directory; added simkit.xml

--- buildoc.bat
+++ buildoc.bat
@@ -1,6 +1,6 @@
 @echo off
-if not exist simsystem mkdir simsystem
-if not exist simsystem\doc mkdir simsystem\doc
+if not exist build mkdir build
+if not exist build\doc mkdir build\doc
 
-javadoc -author -private -d simsystem\doc simkit simkit.stat simkit.random simkit.util simkit.examples simkit.smdx
+javadoc -author -private -d build\doc simkit simkit.stat simkit.random simkit.util simkit.examples simkit.smdx simkit.xml
 
