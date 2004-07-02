32e615834c1bc05d4645b6139401923e08036ba0
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-07-02 00:58:27 +0000

    Replaced by Ant build file

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call buildzip
-call buildsrc
-call buildoc
-
-java -cp build\simkit.jar simkit.Version
-pause
--- buildoc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if not exist build mkdir build
-if not exist build\doc mkdir build\doc
-
-javadoc -author -private -d build\doc simkit simkit.stat simkit.random simkit.util simkit.examples simkit.smdx simkit.xml
-
