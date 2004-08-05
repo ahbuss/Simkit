cbd65bc3fc74dfe7b475cde84f51309ae53726be
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-08-05 21:55:03 +0000

    Test of simkit.util.Resetter class

---
+++ src/simkit/test/TestResetter.java
@@ -0,0 +1,62 @@
+package simkit.test;
+import simkit.*;
+import simkit.stat.*;
+import simkit.random.*;
+import simkit.examples.*;
+import simkit.util.*;
+import simkit.util.Resetter;
+/**
+ *
+ * @author  ahbuss
+ */
+public class TestResetter {
+    
+    public void myResetMethod() {
+        System.out.println("In myResetMethod()!");
+    }
+    
+    public void reset() {
+        System.out.println("In reset()!");
+    }
+    
+    public static void main(String[] args) {
+        RandomVariate iat = RandomVariateFactory.getInstance(
+        "Uniform", new Object[] { new Double(1.2), new Double(3.4) }
+        );
+        ArrivalProcess arrival = new ArrivalProcess(iat);
+        
+        SimpleStatsTimeVarying sstv = new SimpleStatsTimeVarying("numberArrivals");
+        arrival.addPropertyChangeListener(sstv);
+        
+        Resetter resetter = new Resetter();
+        resetter.addResetter(sstv);
+        
+        Object me = new TestResetter();
+        resetter.addResetter(me, "myResetMethod");
+        
+        System.out.println(resetter.getResetters());
+        
+        Schedule.stopAtTime(10000.0);
+        
+        for (int i = 0; i < 3; ++i) {
+            System.out.println("Starting iteration " + i);
+            Schedule.reset();
+            Schedule.startSimulation();
+            System.out.println("\tSimulation ended at time " + Schedule.getSimTime());
+            System.out.println("\tArrivalProcess reports " + arrival.getNumberArrivals() +
+            " arrivals");
+            System.out.println(sstv);
+        }
+        
+        resetter.removeResetter(me);
+        System.out.println(resetter.getResetters());
+
+        Schedule.reset();
+        Schedule.startSimulation();
+        System.out.println("\tSimulation ended at time " + Schedule.getSimTime());
+        System.out.println("\tArrivalProcess reports " + arrival.getNumberArrivals() +
+        " arrivals");
+        System.out.println(sstv);
+    }
+    
+}
