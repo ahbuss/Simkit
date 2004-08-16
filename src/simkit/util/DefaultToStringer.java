780c483026f24e4f6b4de51abd615523d1c182c4
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2004-08-16 20:33:09 +0000

    Seems to work - needs comments

---
+++ src/simkit/util/DefaultToStringer.java
@@ -0,0 +1,38 @@
+package simkit.util;
+import java.beans.*;
+import java.lang.reflect.*;
+/**
+ *
+ * @author  ahbuss
+ */
+public class DefaultToStringer {
+    
+    public static String getToString(Object obj, Class stopClass) {
+        StringBuffer buf = new StringBuffer();
+        if (obj instanceof simkit.Named) {
+            buf.append( ((simkit.Named) obj).getName());
+        }
+        else {
+            buf.append(obj.getClass().getName());
+        }
+        try {
+            BeanInfo info = Introspector.getBeanInfo( obj.getClass(), stopClass );
+            PropertyDescriptor[] descriptors = info.getPropertyDescriptors();
+            for (int i = 0; i < descriptors.length; ++i) {
+                Method readMethod = descriptors[i].getReadMethod();
+                if (readMethod != null) {
+                    buf.append(System.getProperty("line.separator"));
+                    buf.append('\t');
+                    buf.append( descriptors[i].getName() );
+                    buf.append(" = ");
+                    buf.append( readMethod.invoke(obj, null));
+                }
+            }
+        } 
+        catch (IntrospectionException e) { e.printStackTrace(System.err);  }
+        catch (IllegalAccessException e) { e.printStackTrace(System.err);  }
+        catch (InvocationTargetException e) { e.getTargetException().printStackTrace(System.err);  }
+        return buf.toString();
+    }
+    
+}
