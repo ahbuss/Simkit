6fd234fc2e568e8815c9da48a67646c85bf55185
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2005-04-14 23:06:12 +0000

    Added ability to add properties on the fly

--- src/simkit/PropertyChangeDispatcher.java
+++ src/simkit/PropertyChangeDispatcher.java
@@ -1,130 +1,180 @@
 package simkit;
-/////////////////////////// Copyright Notice //////////////////////////
-//                                                                   //
-// This simkit package or sub-package and this file is Copyright (c) //
-// 1997, 1998, 1999 by Kirk A. Stork and Arnold H. Buss.             //
-//                                                                   //
-// Please forward any changes, comments or suggestions to:           //
-//   abuss@nps.navy.mil                                              //
-//                                                                   //
-///////////////////////////////////////////////////////////////////////
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-import java.beans.PropertyDescriptor;
+import java.beans.*;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
+
+import java.util.*;
 
 /**
- *  <P> A delegate for handling properties for an entity. 
+ *  <P> A delegate for handling properties for an entity.
  *  Uses reflection to set/get properties by name (String).  This will also
  *  fire a PropertyChangeEvent whenever a property is set/gotten using the
  *  setProperty() or getProperty() methods here.
  *  @version $Id$
-**/
+ **/
 public class PropertyChangeDispatcher extends PropertyChangeSupport implements PropertyChangeSource {
-
-/**
-* Used internally to hold an instance of java.lang.Object.class.
-**/
-    private static final Class STOP_CLASS;
-
-/**
-* A two dimensional Hash Table to cache information about the properties
-* of classes indexed by class and property name. The information is stored 
-* as a PropertyDescriptor which includes
-* information like the name of the setter and getter of a property.
-**/
-    private static simkit.util.Hashtable2 properties;
-    static {
-        STOP_CLASS = java.lang.Object.class;
-        properties = new simkit.util.Hashtable2();
-    }
-
-/**
-* The entity for which this PropertyChangeDispatcher manages properties.
-**/
+    
+    /**
+     * Used internally to hold an instance of java.lang.Object.class.
+     **/
+    private static final Class STOP_CLASS = java.lang.Object.class;
+    
+    /**
+     * The entity for which this PropertyChangeDispatcher manages properties.
+     **/
     private Object source;
-
-/**
-* Currently unused.
-**/
-    private PropertyChangeListener pcl; //appears unused
-
-/**
-* Create a new PropertyChangeDispatcher to manage properties for the given
-* Object. The properties of the Object are automatically discovered
-* using introspection.
-**/
-    public PropertyChangeDispatcher(Object bean) {
+    
+    private HashMap addedProperties;
+    
+    private HashMap setters;
+    
+    private HashMap getters;
+    
+    /**
+     * Create a new PropertyChangeDispatcher to manage properties for the given
+     * Object. The properties of the Object are automatically discovered
+     * using introspection.
+     **/
+    public PropertyChangeDispatcher(Object bean, Class stopClass) {
         super(bean);
         source = bean;
         Class beanClass = source.getClass();
-        if (!properties.containsKey(beanClass)) {
-            try {
-                BeanInfo bi = Introspector.getBeanInfo(bean.getClass(), STOP_CLASS);
-                PropertyDescriptor[] pd = bi.getPropertyDescriptors();
-                for (int i = 0; i < pd.length; i++) {
-                    properties.put(beanClass, pd[i].getName(), pd[i]);
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass(), stopClass);
+            PropertyDescriptor[] pd = beanInfo.getPropertyDescriptors();
+            setters = new HashMap(pd.length);
+            getters = new HashMap(pd.length);
+            for (int i = 0; i < pd.length; i++) {
+                Method setter = pd[i].getWriteMethod();
+                if (setter != null) {
+                    setters.put(pd[i].getName(), setter);
+                }
+                Method getter = pd[i].getReadMethod();
+                if (getter != null) {
+                    getters.put(pd[i].getName(), getter);
                 }
             }
-            catch (IntrospectionException e) {System.err.println(e);}
         }
+        catch (IntrospectionException e) {
+            throw new RuntimeException(e);
+        }
+        addedProperties = new HashMap();
     }
-
-/**
-* Set the specified property to the given value, firing a PropertyChangeEvent
-* to inform any registered listeners of the change.
-* @throws NullPointerException If the property doesn't exist.
-**/
+    
+    /**
+     * Set the specified property to the given value, firing a PropertyChangeEvent
+     * to inform any registered listeners of the change.
+     * If property is not in the class, it is added to the addedProperties
+     * HashMap.
+     **/
     public void setProperty(String propertyName, Object propertyValue) {
-        PropertyDescriptor pd = (PropertyDescriptor) properties.get(source.getClass(), propertyName);
-        Method setter = pd.getWriteMethod();
-        if (setter == null) { return; }
-        try {
-             setter.invoke(source, new Object[] {propertyValue});
-             this.firePropertyChange(propertyName, null, propertyValue);
+        Method setter = (Method) setters.get(propertyName);
+        if (setter != null ) {
+            try {
+                setter.invoke(source, new Object[] {propertyValue});
+                this.firePropertyChange(propertyName, null, propertyValue);
+            }
+            catch (InvocationTargetException e) {
+                throw new RuntimeException(e.getTargetException());
+            }
+            catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
         }
-        catch (InvocationTargetException e) {
-            System.err.println(e.getTargetException());
+        else {
+            addedProperties.put(propertyName, propertyValue);
         }
-        catch (IllegalAccessException e) {System.err.println(e);}
     }
-
-/**
-* Get the value of the given property, firing a PropertyChangeEvent
-* to inform any registered listeners of the current value of the 
-* property.
-* @throws NullPointerException If the property doesn't exist.
-**/
+    
+    public PropertyChangeDispatcher(Object bean) {
+        this(bean, STOP_CLASS);
+    }
+    
+    /**
+     * Get the value of the given property,  First the class's
+     * properties are tried, then the addedProperties.
+     * @return property value (or null if property doesn't exist)
+     **/
     public Object getProperty(String propertyName) {
-        PropertyDescriptor pd = (PropertyDescriptor) properties.get(source.getClass(), propertyName);
-        Method getter = pd.getReadMethod();
-        if (getter == null) { return null; }
         Object result = null;
-        try {
-             result = getter.invoke(source, (Object[]) null);
-             firePropertyChange(propertyName, null, result);
+        Method getter = (Method) getters.get(propertyName);
+        if (getter != null) {
+            try {
+                result = getter.invoke(source, (Object[]) null);
+            }
+            catch (InvocationTargetException e) {
+                throw new RuntimeException(e.getTargetException());
+            }
+            catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
         }
-        catch (InvocationTargetException e) {
-            System.err.println(e.getTargetException());
+        else {
+            result = addedProperties.get(propertyName);
         }
-        catch (IllegalAccessException e) {System.err.println(e);}
         return result;
     }
-
-/**
-* Get the value of the given property or return the given default.
-* @return The value of the property if found and not null, otherwise the 
-* default value.
-* @throws NullPointerException If the property doesn't exist.
-**/
+    
+    /**
+     * Get the value of the given property or return the given default.
+     * @return The value of the property if found and not null, otherwise the
+     * default value.
+     * @throws NullPointerException If the property doesn't exist.
+     **/
     public Object getProperty(String propertyName, Object defaultValue) {
         Object prop = getProperty(propertyName);
         return (prop != null) ? prop : defaultValue;
     }
-
-} 
+    
+    public void clearAddedProperties() {
+        addedProperties.clear();
+    }
+    
+    public String toString() {
+        StringBuffer buf = new StringBuffer();
+        for (Iterator i = getters.keySet().iterator(); i.hasNext(); ) {
+            Object property = i.next();
+            
+            Object value = getProperty(property.toString());
+            if (value == null || !value.getClass().isArray() ) {
+                buf.append(System.getProperty("line.separator"));
+                buf.append(property);
+                buf.append(" = ");
+                buf.append(value);
+            }
+            else {
+                for (int j = 0; j < Array.getLength(value); ++j) {
+                    buf.append(System.getProperty("line.separator"));
+                    buf.append(property);
+                    buf.append('[');
+                    buf.append(j);
+                    buf.append("] = ");
+                    buf.append(Array.get(value, j));
+                }
+            }
+        }
+        for (Iterator i = addedProperties.keySet().iterator(); i.hasNext(); ) {
+            Object property = i.next();
+            Object value = addedProperties.get(property);
+            if (value == null || !value.getClass().isArray() ) {
+                buf.append(System.getProperty("line.separator"));
+                buf.append(property);
+                buf.append(" = ");
+                buf.append(value);
+            }
+            else {
+                for (int j = 0; j < Array.getLength(value); ++j) {
+                    buf.append(System.getProperty("line.separator"));
+                    buf.append(property);
+                    buf.append('[');
+                    buf.append(j);
+                    buf.append("] = ");
+                    buf.append(Array.get(value, j));
+                }
+            }
+        }
+        return buf.toString();
+    }
+    
+}
