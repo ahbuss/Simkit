1ce72b2d88a9039eaf6312e2d0b4e766ea8ed73d
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2005-05-12 19:54:48 +0000

    Test of new added properties functionality

---
+++ src/simkit/test/TestAddedProperties.java
@@ -0,0 +1,25 @@
+package simkit.test;
+import simkit.*;
+/**
+ * @version $Id$
+ * @author  ahbuss
+ */
+public class TestAddedProperties extends SimEntityBase {
+    
+    private int foobar;
+    
+    public void setFoobar(int f)  {foobar = f; }
+    
+    public int getFoobar() { return foobar; }
+    
+    public static void main(String[] args) {
+        SimEntity taps = new TestAddedProperties();
+        
+        taps.setProperty("foo", "Bar");
+        taps.setProperty("one", new Integer(1));
+        taps.setProperty("array", new double[] { 1, 2, 3.4 });
+        
+        System.out.println(taps);
+    }
+    
+}
