a36ab8d6a61228c4d745b2a2cb7ade8feef825cb
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2005-09-08 02:01:35 +0000

    Immutable copy of SampleStatistics for saving

---
+++ src/simkit/stat/SavedStats.java
@@ -0,0 +1,37 @@
+package simkit.stat;
+
+import simkit.stat.AbstractSimpleStats;
+import simkit.stat.SampleStatistics;
+import simkit.stat.SamplingType;
+
+/**
+ * @version $Id$
+ * @author ahbuss
+ */
+public class SavedStats extends AbstractSimpleStats {
+    
+    protected double mean;
+    protected double variance;
+    protected SamplingType samplingType;
+    
+    public SavedStats(SampleStatistics original) {
+        this.name = original.getName();
+        this.count = original.getCount();
+        this.minObs = original.getMinObs();
+        this.maxObs = original.getMaxObs();
+        this.mean = original.getMean();
+        this.variance = original.getVariance();
+        this.samplingType = original.getSamplingType();
+    }
+    
+    public double getMean() { return mean; }
+    
+    public simkit.stat.SamplingType getSamplingType() { return samplingType; }
+    
+    public double getStandardDeviation() {
+        return Math.sqrt(getVariance());
+    }
+    
+    public double getVariance() { return variance; }
+    
+}
