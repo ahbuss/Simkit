aa21108b3ef24da47cd37dc1eb6dbee5205f4cb7
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2006-04-18 16:29:26 +0000

    For unit testing purposes.  Allows determination of whether a given event has been scheduled.

---
+++ src/simkit/util/UnitTestEventList.java
@@ -0,0 +1,123 @@
+package simkit.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import simkit.EventList;
+import simkit.SimEntity;
+import simkit.SimEvent;
+import simkit.SimEventComp;
+
+/**
+ * <p>This EventList implementation is for Unit Testing purposes only.
+ * Specifically, it only stores scheduled events and returns a copy of
+ * them, the first scheduled SimEvent of a given name, or an array of
+ * SimEvents of a given name.  Since no SimEvents are passed to the
+ * superclass scheduleEvent() method, nothing will ever be run.
+ * <p>Instantiation should be left to Schedule, and all SimEntities to
+ * be tested should have their simEventListID set to the id that is
+ * given it by Schedule.
+ * <p>Usage:
+ * <pre>
+ *          int id = Schedule.addNewEventList(UnitTestEventList.class);
+ *          ...
+ *          SimEntity aSimEntity = new ASimEntity(...);
+ *          aSimEntity.setEventListID(id);
+ * </pre>
+ * <p>After each test, a call to coldReset() on the UnitTestEventList instance
+ * should be made.  Note that Schedule.coldReset() only resets its default
+ * EventList instance.
+ *
+ * @version $Id$
+ * @author ahbuss
+ */
+public class UnitTestEventList extends EventList {
+    
+    /**
+     * Instantiate a UnitTestEventList with the given id number.
+     * @param the id of this UnitTestEventList instance
+     */
+    public UnitTestEventList(int id) {
+        super(id);
+    }
+
+    /**
+     * Gets the SimEvent of the given name that would occur first in the
+     * current event list's state.
+     * @param eventName Name of desired SimEvent
+     * @return SimEvent of given name that would happen first or null if
+     * no such event
+     */
+    public SimEvent getScheduledEvent(String eventName) {
+        SimEvent simEvent = null;
+        SortedSet eventListCopy = getEventList();
+        for (Iterator i = eventListCopy.iterator(); i.hasNext(); ) {
+            SimEvent nextEvent = (SimEvent) i.next();
+            if (nextEvent.getEventName().equals(eventName)) {
+                simEvent = nextEvent;
+                break;
+            }
+        }
+        return simEvent;
+    }
+    
+    /**
+     * Gets the SimEvent of the given name and parameter list that would occur
+     * first in the current event list's state.
+     * @param eventName Name of desired SimEvent
+     * @param params parameters passed to event when scheduled
+     * @return SimEvent of given name that would happen first or null if
+     * no such event
+     */
+    public SimEvent getScheduledEvent(String eventName, Object[] params) {
+        SimEvent simEvent = null;
+        SortedSet eventListCopy = getEventList();
+        for (Iterator i = eventListCopy.iterator(); i.hasNext(); ) {
+            SimEvent nextEvent = (SimEvent) i.next();
+            if (nextEvent.getEventName().equals(eventName) &&
+                    nextEvent.interruptParametersMatch(params)) {
+                simEvent = nextEvent;
+                break;
+            }
+        }
+        return simEvent;
+    }
+    
+    /**
+     * Returns all scheduled events of the given name in increasing scheduled
+     * order.
+     * @param eventName Name of the desired SimEvent
+     * @return Array of all scheduled SimEvents of the given name
+     */
+    public SortedSet getScheduledEvents(String eventName) {
+        SortedSet simEvents = new TreeSet(new SimEventComp());
+        SortedSet eventListCopy = getEventList();
+        for (Iterator i = eventListCopy.iterator(); i.hasNext(); ) {
+            SimEvent nextEvent = (SimEvent) i.next();
+            if (nextEvent.getEventName().equals(eventName)) {
+                simEvents.add(nextEvent);
+            }
+        }
+        return simEvents;
+    }
+    
+    /**
+     * Returns all scheduled events of the given name and parameter list in
+     * increasing scheduled order.
+     * @param eventName Name of the desired SimEvent
+     * @return Array of all scheduled SimEvents of the given name
+     */
+    public SortedSet getScheduledEvents(String eventName, Object[] params) {
+        SortedSet simEvents = new TreeSet(new SimEventComp());
+        SortedSet eventListCopy = getEventList();
+        for (Iterator i = eventListCopy.iterator(); i.hasNext(); ) {
+            SimEvent nextEvent = (SimEvent) i.next();
+            if (nextEvent.getEventName().equals(eventName) &&
+                    nextEvent.interruptParametersMatch(params)) {
+                simEvents.add(nextEvent);
+            }
+        }
+        return simEvents;
+    }   
+}
