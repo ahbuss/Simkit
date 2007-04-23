729063b698909b74365d3008cdd6e4fc449e6fd7
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2007-04-23 18:44:51 +0000

    Made Comparable, with default order based on timeStamp property

--- src/simkit/Entity.java
+++ src/simkit/Entity.java
@@ -7,13 +7,25 @@ import simkit.Named;
 import simkit.Schedule;
 
 /**
- * A generic entity that can be used as a Customer, a Job, etc. It can be subclassed
+ * <p>A generic entity that can be used as a Customer, a Job, etc. It can be subclassed
  * to add features or properties, or additional properties can be added dynamically.
+ *
+ * <p>Entity has been made "Comparable" so that it can easily be put into
+ * sorted containers, such as a TreeSet.  The default ordering is by the
+ * timeStamp property, with ties broken by the id property.  There is some
+ * danger to this because the timeStamp property is mutable, and therefore
+ * bad things could happen if it were modified on an instance while it was
+ * in a sorted container.  Therefore, care must be taken so that the
+ * stampTime() method is only invoked when the instance is <i>not</i>
+ * in a sorted container.  Usually there will not be a need to do this, but
+ * if it is absolutely necessary, then the instance should first be removed 
+ * from the container, then stampTime() invoked, and finally the instance put 
+ * back on the container.
  *
  * @version $Id$
  * @author ahbuss
  */
-public class Entity implements Named {
+public class Entity implements Named, Comparable<Entity> {
     
     public static final DecimalFormat FORM = new DecimalFormat("0.0000");
     
@@ -182,5 +194,27 @@ public class Entity implements Named {
             }
         }
         return buf.toString();
+    }
+
+    /**
+     * Higher priority is based on timeStamp property, with ties broken
+     * by id property.  A smaller timeStamp gives higher priority, effectively
+     * making the default ordering "FCFS".  As noted in the class comments,
+     * care must be taken to not invoke stampTime() while an instance of Entity
+     * is contained in a SortedSet.  Normally, stampTime() is called before
+     * putting in the set.
+     *
+     * @param o Entity to be compared with
+     * @return -1 if this has higher priority than o, -1 if this has lower
+     * priority than o, 0 if they are the same entity
+     */
+    public int compareTo(Entity o) {
+        if (this.getTimeStamp() < o.getTimeStamp()) {
+            return -1;
+        } else if (this.getTimeStamp() > o.getTimeStamp()) {
+            return 1;
+        } else {
+            return this.getID() - o.getID();
+        }
     }
 }
