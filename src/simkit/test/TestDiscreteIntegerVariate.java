5d6c59012787248151e7ac7fdbccb61655bbaa07
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2007-08-01 22:35:41 +0000

    Added DiscreteRandomVariate for arbitratry integer-valued distributions

---
+++ src/simkit/test/TestDiscreteIntegerVariate.java
@@ -0,0 +1,31 @@
+package simkit.test;
+
+import simkit.random.DiscreteRandomVariate;
+import simkit.random.RandomVariateFactory;
+import simkit.stat.Histogram;
+
+/**
+ * @version $Id$
+ * @author ahbuss
+ */
+public class TestDiscreteIntegerVariate {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        RandomVariateFactory.addSearchPackage("random");
+        DiscreteRandomVariate rv = 
+                RandomVariateFactory.getDiscreteRandomVariateInstance(
+                "DiscreteInteger",  new int[] { 0, 5 },
+                    new double[] {1.0, 3.0 } );
+        System.out.println(rv);
+        
+        Histogram histogram = new Histogram("Discrete Generated", 0, 5, 5);
+        for (int i = 0; i < 100000; ++i) {
+            histogram.newObservation(rv.generateInt());
+        }
+        System.out.println(histogram);
+    }
+}
