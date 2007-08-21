73221216b81c5525bfebf02da2646fbe361817b3
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2007-08-21 17:07:58 +0000

    Listens for PropertyChangeEvents, caches the last value by property.  When EndIterationevent is heard, gather values in SimpleStatsTally, keyes by property name

---
+++ src/simkit/stat/MultipleEndingStateStatTally.java
@@ -0,0 +1,72 @@
+package simkit.stat;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.Map;
+import simkit.SimEntityBase;
+import simkit.stat.SimpleStatsTally;
+
+/**
+ * @version $Id$
+ * @author ahbuss
+ */
+public class MultipleEndingStateStatTally extends SimEntityBase implements 
+        PropertyChangeListener {
+    
+    protected Map<String, Number> lastStateValue;
+    
+    protected Map<String, SimpleStatsTally> lastStateValueStat;
+    
+    
+    public MultipleEndingStateStatTally() {
+        lastStateValue = new HashMap<String, Number>();
+        lastStateValueStat = new HashMap<String, SimpleStatsTally>();
+    }
+
+    public void reset() {
+        super.reset();
+        lastStateValue.clear();
+    }
+
+    public void doRun() {
+    }
+    
+    public void deepReset() {
+        this.reset();
+        lastStateValueStat.clear();
+    }
+    
+    public void doEndIterationEvent() {
+        for (String property : lastStateValue.keySet()) {
+            double lastValue = getLastStateValueFor(property);
+            if (!Double.isNaN(lastValue)) {
+                SimpleStatsTally stat = lastStateValueStat.get(property);
+                if (stat == null) {
+                    stat = new SimpleStatsTally(property);
+                    lastStateValueStat.put(property, stat);
+                }
+                stat.newObservation(lastValue);
+            }
+        }
+    }
+    
+    public void propertyChange(PropertyChangeEvent e) {
+        if (java.lang.Number.class.isAssignableFrom(e.getNewValue().getClass())) {
+            lastStateValue.put(e.getPropertyName(), (Number) e.getNewValue());
+        }
+    }
+
+    public Map<String, Number> getLastStateValue() {
+        return new HashMap<String, Number>(lastStateValue);
+    }
+
+    public Map<String, SimpleStatsTally> getLastStateValueStat() {
+        return new HashMap<String, SimpleStatsTally>(lastStateValueStat);
+    }
+    
+    public double getLastStateValueFor(String property) {
+        Number lastValue = lastStateValue.get(property);
+        return lastValue != null ? lastValue.doubleValue() : Double.NaN;
+    }
+}
