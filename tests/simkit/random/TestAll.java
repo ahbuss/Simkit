c60229c17d43ca65fc356cbc3e42218f200d27bb
jlruck <jlruck@c6e24747-a972-dc11-9d74-0011435b89d6>
2007-08-24 17:44:44 +0000

    Added junit tests for LogNormalVariate.

--- tests/simkit/random/TestAll.java
+++ tests/simkit/random/TestAll.java
@@ -6,6 +6,7 @@ public class TestAll extends TestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite("TestAll");
         suite.addTest(new TestSuite(MersenneTwisterFactoryTest.class));
+        suite.addTest(new TestSuite(LogNormalTest.class));
         return suite;
     }
 }
