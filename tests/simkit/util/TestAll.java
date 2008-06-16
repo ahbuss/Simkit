3338953bff59e7d6d3b00e9e33419bb1658d215d
jlruck <jlruck@c6e24747-a972-dc11-9d74-0011435b89d6>
2008-06-16 21:28:01 +0000

    Fixed missing tests. Test counts now match with 1 known failure due to bug 1315.

--- tests/simkit/util/TestAll.java
+++ tests/simkit/util/TestAll.java
@@ -6,6 +6,7 @@ public class TestAll extends TestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite("TestAll");
         suite.addTest(new TestSuite(Hashtable2Test.class));
+        suite.addTest(new TestSuite(JackcessTableReadTest.class));
         return suite;
     }
 }
