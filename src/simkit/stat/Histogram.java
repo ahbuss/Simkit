c5b2f777408be5a7bba0733bed56a30404b74442
ahbuss <ahbuss@c6e24747-a972-dc11-9d74-0011435b89d6>
2016-05-31 20:23:46 +0000

    Updated Histogram class to gave getters for data; modified toString output to give the adjusted frequencies - instead of simply summing to 1, they have area of 1, and so are a closer match to a pdf.

--- src/simkit/stat/Histogram.java
+++ src/simkit/stat/Histogram.java
@@ -1,70 +1,85 @@
 package simkit.stat;
 
-import java.text.DecimalFormat;
-
 /**
-* A class that collects statistics that can be used to produce a histogram.
-* This class does not draw the histogram, it simply calculates frequency and
-* proportion data.
-* @version $Id$
-**/
+ * A class that collects statistics that can be used to produce a histogram.
+ * This class does not draw the histogram, it simply calculates frequency and
+ * proportion data.
+ *
+ * @version $Id$
+*
+ */
 public class Histogram extends SimpleStatsTally {
 
-/**
-* Contains the number of observations that fall into each bin.
-**/
+    private double[] values;
+
+    /**
+     * Contains the number of observations that fall into each bin.
+*
+     */
     private int[] frequency;
 
-/**
-* Contains the total number of observations.
-**/
+    /**
+     * Contains the total number of observations.
+*
+     */
     private int total;
 
-/**
-* Contains the lower bound of the first bin.
-**/
+    /**
+     * Contains the lower bound of the first bin.
+*
+     */
     private double lowValue;
 
-/**
-* Contains the bin width.
-**/
+    /**
+     * Contains the bin width.
+*
+     */
     private double cellWidth;
 
-/**
-* Creates a new Histogram with a lower bound of 0 and a
-* bin width of 1. This implies that the upper bound is equal to
-* the number of bins.
-* @param name The name of the property for which this Histogram collects statistics.
-* @param number The number of bins.
-**/
+    /**
+     * Creates a new Histogram with a lower bound of 0 and a bin width of 1.
+     * This implies that the upper bound is equal to the number of bins.
+     *
+     * @param name The name of the property for which this Histogram collects
+     * statistics.
+     * @param number The number of bins.
+*
+     */
     public Histogram(String name, int number) {
-        this(name, 0, (double) number, number);
+        this(name, 0, number, number);
     }
 
-/**
-* Creates a new Histogram.
-* @param name The name of the property for which this Histogram collects statistics.
-* @param lowVal The lower bound of the first bin.
-* @param hiVal The upper bound of the last bin.
-* @param number The number of bins.
-**/
+    /**
+     * Creates a new Histogram.
+     *
+     * @param name The name of the property for which this Histogram collects
+     * statistics.
+     * @param lowVal The lower bound of the first bin.
+     * @param hiVal The upper bound of the last bin.
+     * @param number The number of bins.
+*
+     */
     public Histogram(String name, double lowVal, double hiVal, int number) {
         super(name);
         frequency = new int[number + 1];
         lowValue = lowVal;
-        cellWidth = (hiVal - lowVal) /  number;
+        cellWidth = (hiVal - lowVal) / number;
+        values = new double[frequency.length];
+        for (int i = 0; i < frequency.length; ++i) {
+            values[i] = lowVal + i * cellWidth;
+        }
     }
 
-//Javadoc inherited.    
+    @Override
     public void newObservation(double x) {
         super.newObservation(x);
-        int index = (int) Math.floor(( x - lowValue) / cellWidth);
-        index = Math.min( Math.max(index, 0), frequency.length - 1);
+        int index = (int) Math.floor((x - lowValue) / cellWidth);
+        index = Math.min(Math.max(index, 0), frequency.length - 1);
         frequency[index]++;
         total++;
     }
-    
-//Javadoc inherited.    
+
+    @Override
     public void reset() {
         if (frequency != null) {
             frequency = new int[frequency.length];
@@ -72,17 +87,19 @@ public class Histogram extends SimpleStatsTally {
         total = 0;
     }
 
-/**
-* Returns a copy of the array of bin frequencies.
-**/
-    public int[] getFrequencies() { return (int[]) frequency.clone(); }
+    /**
+     * @return a copy of the array of bin frequencies
+     */
+    public int[] getFrequencies() {
+        return frequency.clone();
+    }
 
-/**
-* Returns an array containing the proportion of observations in each bin.
-* Note that if any observations were less than the lower bound or 
-* greater than the upper bound, then the proportions will not add
-* up to 1.
-**/
+    /**
+     * Note that if any observations were less than the lower bound or greater
+     * than the upper bound, then the proportions will not add up to 1.
+     *
+     * @return an array containing the proportion of observations in each bin.
+     */
     public double[] getProportions() {
         double[] temp = new double[frequency.length];
         for (int i = 0; i < temp.length; i++) {
@@ -90,26 +107,63 @@ public class Histogram extends SimpleStatsTally {
         }
         return temp;
     }
+    
+    public double[] getScaledProportions() {
+        double[] scaled = getProportions();
+        for (int i = 0; i < scaled.length; ++i) {
+            scaled[i] /= cellWidth;
+        }
+        return scaled;
+    }
 
-/**
-* Returns a table with the frequencies and proportions in each bin.
-**/
+    @Override
     public String toString() {
-        DecimalFormat form = new DecimalFormat("0.000");
         StringBuilder buf = new StringBuilder(super.toString());
-        buf.append("\nvalue\tFrequencies\tProportions\n");
-        double[] prop = getProportions();
-        for (int i = 0; i < frequency.length; i++ ) {
-//            buf.append(i);
-//            buf.append('\t');
-            buf.append(form.format(lowValue + cellWidth * i));
-            buf.append('\t');
-            buf.append(frequency[i]);
-            buf.append('\t');
-            buf.append(prop[i]);
-            buf.append(SampleStatistics.EOL);
+        buf.append(EOL);
+        buf.append("value\tFrequencies\tProportions");
+        double[] scaledProportions = getScaledProportions();
+        for (int i = 0; i < frequency.length; i++) {
+            buf.append(EOL);
+            buf.append(String.format("%.3f\t", values[i]));
+            buf.append(String.format("%d\t\t", frequency[i]));
+            buf.append(String.format("%.3f", scaledProportions[i]));
         }
         return buf.toString();
+    }
+
+    /**
+     * @return the values
+     */
+    public double[] getValues() {
+        return values.clone();
+    }
+
+    /**
+     * @return the frequency
+     */
+    public int[] getFrequency() {
+        return frequency.clone();
+    }
+
+    /**
+     * @return the total
+     */
+    public int getTotal() {
+        return total;
+    }
+
+    /**
+     * @return the lowValue
+     */
+    public double getLowValue() {
+        return lowValue;
+    }
+
+    /**
+     * @return the cellWidth
+     */
+    public double getCellWidth() {
+        return cellWidth;
     }
 
 }
